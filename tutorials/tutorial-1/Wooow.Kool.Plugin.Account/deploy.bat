ef66929f768acc2d36a51937babe0afce92aa5e8
PhuocLe <vanphuoc@gmail.com>
2018-03-06 17:52:08 +0700

    Add finished tutorial 1

---
+++ tutorials/tutorial-1/Wooow.Kool.Plugin.Account/deploy.bat
@@ -0,0 +1,11 @@
+@echo off
+set MsBuild="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+call %MsBuild% /nologo /noautorsp /verbosity:minimal /p:Configuration=Debug Wooow.Kool.Plugin.Account.csproj
+for /f "delims=" %%d in ('dir /a:d /o:-n /s ..\packages\PL.DynamicsCrm.DevKit.Cli.* /b') do (
+    set PLDynamicsCrmDevKitCli=%%d
+    goto break
+)
+:break
+set CrmConnection="AuthType=Office365;Url=https://xxx.crm.dynamics.com;Username=admin@xxx.onmicrosoft.com;Password=xxx;"
+"%PLDynamicsCrmDevKitCli%\tools\PL.DynamicsCrm.DevKit.Cli.exe" /conn:%CrmConnection% /json:"..\PL.DynamicsCrm.DevKit.Cli.json" /type:"plugins" /profile:"DEBUG" /version:"1.0.0.0"
+exit
