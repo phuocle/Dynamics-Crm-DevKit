9ee33a534f846955d0ee6f918a2b38ed57439eae
PhuocLe <vanphuoc@gmail.com>
2018-08-24 16:26:22 +0700

    Finish tutorial 2

---
+++ tutorials/Paz.LuckeyMonkey.Plugin.Lead.Test/PreLeadCreateSynchronousTest.cs
@@ -0,0 +1,132 @@
+﻿using FakeXrmEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk;
+using Paz.LuckeyMonkey.Shared;
+using Paz.LuckeyMonkey.Plugin.Lead;
+using System.Reflection;
+using Paz.LuckeyMonkey.ProxyTypes;
+using System;
+
+namespace Paz.LuckeyMonkey.Plugin
+{
+    [TestClass]
+    public class PreLeadCreateSynchronousTest
+    {
+        public static XrmFakedContext Context { get; set; }
+        public static XrmFakedPluginExecutionContext PluginContext { get; set; }
+
+        [ClassInitialize()]
+        public static void ClassInit(TestContext context)
+        {
+            Context = new XrmFakedContext();
+            PluginContext = Context.GetDefaultPluginContext();
+            PluginContext.MessageName = "Create";
+            PluginContext.Stage = (int)StageEnum.PreOperation;
+            PluginContext.Mode = (int)ExecutionModeEnum.Synchronous;
+            PluginContext.InputParameters["Target"] = null;
+        }
+
+        [TestMethod]
+        public void PreLeadCreate_Stage_Does_Not_Equals_PreOperation()
+        {
+            var context = new XrmFakedContext();
+            var plugin = context.GetDefaultPluginContext();
+            plugin.Stage = -1;
+            Assert.ThrowsException<InvalidPluginExecutionException>(() => context.ExecutePluginWithConfigurations<PreLeadCreateSynchronous>(plugin, null, null), "Stage does not equals PreOperation");
+        }
+
+        [TestMethod]
+        public void PreLeadCreate_PrimaryEntityName_Does_Not_Equals_Lead()
+        {
+            var context = new XrmFakedContext();
+            var plugin = context.GetDefaultPluginContext();
+            plugin.Stage = (int)StageEnum.PreOperation;
+            plugin.PrimaryEntityName = "abcd";
+            Assert.ThrowsException<InvalidPluginExecutionException>(() => context.ExecutePluginWithConfigurations<PreLeadCreateSynchronous>(plugin, null, null), "PrimaryEntityName does not equals lead");
+        }
+
+        [TestMethod]
+        public void PreLeadCreate_MessageName_Does_Not_Equals_Create()
+        {
+            var context = new XrmFakedContext();
+            var plugin = context.GetDefaultPluginContext();
+            plugin.Stage = (int)StageEnum.PreOperation;
+            plugin.PrimaryEntityName = "lead";
+            plugin.MessageName = "abcd";
+            Assert.ThrowsException<InvalidPluginExecutionException>(() => context.ExecutePluginWithConfigurations<PreLeadCreateSynchronous>(plugin, null, null), "MessageName does not equals Create");
+        }
+
+        [TestMethod]
+        public void PreLeadCreate_Mode_Does_Not_Equals_Synchronous()
+        {
+            var context = new XrmFakedContext();
+            var plugin = context.GetDefaultPluginContext();
+            plugin.Stage = (int)StageEnum.PreOperation;
+            plugin.PrimaryEntityName = "lead";
+            plugin.MessageName = "Create";
+            plugin.Mode = -1;
+            Assert.ThrowsException<InvalidPluginExecutionException>(() => context.ExecutePluginWithConfigurations<PreLeadCreateSynchronous>(plugin, null, null), "Execution does not equal Synchronous");
+        }
+
+        /*
+        [TestMethod]
+        public void PreLeadCreate_CrmPluginRegistration_Check_Image1()
+        {
+            var @class = new PreLeadCreateSynchronous();
+            foreach (var attribute in Attribute.GetCustomAttributes(@class.GetType()))
+            {
+                if (attribute.GetType().Equals(typeof(CrmPluginRegistrationAttribute)))
+                {
+                    var check = attribute as CrmPluginRegistrationAttribute;
+                    Assert.IsNotNull(check.Image1Attributes);
+                    Assert.IsNotNull(check.Image1Name);
+                    Assert.IsNotNull(check.Image1Type);
+                }
+                else
+                    Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void PreLeadCreate_CrmPluginRegistration_Check_Image2()
+        {
+            var @class = new PreLeadCreateSynchronous();
+            foreach (var attribute in Attribute.GetCustomAttributes(@class.GetType()))
+            {
+                if (attribute.GetType().Equals(typeof(CrmPluginRegistrationAttribute)))
+                {
+                    var check = attribute as CrmPluginRegistrationAttribute;
+                    Assert.IsNotNull(check.Image2Attributes);
+                    Assert.IsNotNull(check.Image2Name);
+                    Assert.IsNotNull(check.Image2Type);
+                }
+                else
+                    Assert.Fail();
+            }
+        }
+        */
+
+        [TestMethod]
+        public void PreLeadCreate_LeadSubjectAlwaysUppercase()
+        {
+            //setup
+            Context.ProxyTypesAssembly = Assembly.GetAssembly(typeof(ProxyTypesAssembly));
+            Context.Data.Clear();
+            var subject = "lowcase subject";
+            var target = new Entity("lead")
+            {
+                ["leadid"] = Guid.NewGuid(),
+                ["subject"] = subject
+            };
+            PluginContext.InputParameters["Target"] = target;
+
+            //run
+            Context.ExecutePluginWithConfigurations<PreLeadCreateSynchronous>(PluginContext, null, null);
+
+            //result
+            var resultTarget = (Entity)PluginContext.InputParameters["Target"];
+            var lead = new Shared.Entities.Lead(resultTarget);
+            Assert.AreEqual(subject.ToUpper(), lead.Subject, false);
+        }
+    }
+}
---
+++ tutorials/Paz.LuckeyMonkey.ProxyTypes/run.bat
@@ -0,0 +1,3 @@
+@echo off
+..\packages\Microsoft.CrmSdk.CoreTools.9.0.2.4\content\bin\coretools\CrmSvcUtil.exe /url:https://demodevkit.crm.dynamics.com /username:admin@crm290456.onmicrosoft.com /password:phuoc.levan@1180 /namespace:Paz.LuckeyMonkey.ProxyTypes /out:GeneratedCode.cs
+exit
