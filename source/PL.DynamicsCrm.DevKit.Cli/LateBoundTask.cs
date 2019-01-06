404e3a9d1bba71019fe4ba0cbdf7b93703f1d4b4
Phuoc.Le <vanphuoc@gmail.com>
2019-01-06 22:39:21 +0700

    Save for work later to another laptop

---
+++ source/PL.DynamicsCrm.DevKit.Cli/LateBoundTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Tooling.Connector;
+using PL.DynamicsCrm.DevKit.Cli.Models;
+
+namespace PL.DynamicsCrm.DevKit.Cli
+{
+    class LateBoundTask
+    {
+        private CrmServiceClient crmServiceClient;
+        private string currentDirectory;
+        private LateBound lateBoundJson;
+        private string version;
+
+        public LateBoundTask(CrmServiceClient crmServiceClient, string currentDirectory, LateBound lateBoundJson, string version)
+        {
+            this.crmServiceClient = crmServiceClient;
+            this.currentDirectory = currentDirectory;
+            this.lateBoundJson = lateBoundJson;
+            this.version = version;
+        }
+
+        internal void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
