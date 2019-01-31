47d55113c940da65b66674991ebc85ec8e14018f
PhuocLe <vanphuoc@gmail.com>
2019-01-31 15:53:03 +0700

    Ready for publish

--- source/PL.DynamicsCrm.DevKit.Analyzers/Nuget/pack.bat
+++ source/PL.DynamicsCrm.DevKit.Analyzers/Nuget/pack.bat
@@ -1,3 +1,4 @@
 @echo off
 nuget pack PL.DynamicsCrm.DevKit.Analyzers.nuspec -Tool
+copy PL.DynamicsCrm.DevKit.Analyzers.1.2.0.nupkg ..\..\Published\1.2.0\PL.DynamicsCrm.DevKit.Analyzers.1.2.0.nupkg /y
 exit
--- source/PL.DynamicsCrm.DevKit.Cli/Nuget/pack.bat
+++ source/PL.DynamicsCrm.DevKit.Cli/Nuget/pack.bat
@@ -1,3 +1,5 @@
 @echo off
 nuget pack PL.DynamicsCrm.DevKit.Cli.nuspec -Tool
+copy PL.DynamicsCrm.DevKit.Cli.1.2.0.nupkg ..\..\Published\1.2.0\PL.DynamicsCrm.DevKit.Cli.1.2.0.nupkg /y
+
 exit
