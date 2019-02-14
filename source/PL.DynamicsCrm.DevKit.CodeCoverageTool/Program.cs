f72a2d855a76d3e077b6a8116d999aa4dee5f88a
Phuoc.Le <vanphuoc@gmail.com>
2019-02-14 23:05:31 +0700

    Continue fixed bugs

--- source/PL.DynamicsCrm.DevKit.CodeCoverageTool/Program.cs
+++ source/PL.DynamicsCrm.DevKit.CodeCoverageTool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using Microsoft.VisualStudio.Coverage.Analysis;
 
 namespace PL.DynamicsCrm.DevKit.CodeCoverageTool
@@ -8,6 +9,7 @@ namespace PL.DynamicsCrm.DevKit.CodeCoverageTool
         //"D:\src\vsts\tfs\CDS-CRMGRIDPLUS\CRM\Release\coverage.xml" "D:\src\vsts\tfs\CDS-CRMGRIDPLUS\CRM\ResultsDirectory\5691dd01-3997-4a19-9e7e-1a49cffc745e\phuoclev_HSSSC1LAP02501 2019-02-14 17_02_33.coverage" "D:\src\vsts\tfs\CDS-CRMGRIDPLUS\CRM\Release\test\PL.CrmGridPlus.CustomAction.Test.dll" "D:\src\vsts\tfs\CDS-CRMGRIDPLUS\CRM\Release\test\PL.CrmGridPlus.DataProvider.D365Icons.Test.dll" "D:\src\vsts\tfs\CDS-CRMGRIDPLUS\CRM\Release\test\PL.CrmGridPlus.Plugin.Email.Test.dll" "D:\src\vsts\tfs\CDS-CRMGRIDPLUS\CRM\Release\test\PL.CrmGridPlus.Plugin.pl_Lead.Test.dll" "D:\src\vsts\tfs\CDS-CRMGRIDPLUS\CRM\Release\test\PL.CrmGridPlus.Plugin.pl_License.Test.dll" "D:\src\vsts\tfs\CDS-CRMGRIDPLUS\CRM\Release\test\PL.CrmGridPlus.Workflow.Test.dll"
         static void Main(string[] args)
         {
+            Directory.SetCurrentDirectory(@"D:\src\vsts\tfs\CDS-CRMGRIDPLUS\CRM");
             var coverage = args[0];
             var file = args[1];
             var dlls = new List<string>();
