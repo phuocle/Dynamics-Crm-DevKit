81671a1e0b210744e2072e0d62897f1f918469bb
PhuocLe <vanphuoc@gmail.com>
2019-02-27 20:15:44 +0700

    Continue fixed bugs and testing ...

---
+++ test/CRM2013/Alpha.Beta.SolutionPackager/Extract-Unmanaged.bat
@@ -0,0 +1,9 @@
+@echo off
+for /f "delims=" %%d in ('dir /a:d /o:-n /s ..\packages\PL.DynamicsCrm.DevKit.Cli.* /b') do (
+    set PLDynamicsCrmDevKitCli=%%d
+    goto break
+)
+:break
+set CrmConnection="AuthType=AD;Url=http://crm2013.southeastasia.cloudapp.azure.com/G16;Domain=crm;Username=crmadmin;Password=67ubH5C8nrSvsmDAv/Zixw==;"
+"%PLDynamicsCrmDevKitCli%\tools\PL.DynamicsCrm.DevKit.Cli.exe" /conn:%CrmConnection% /json:"..\PL.DynamicsCrm.DevKit.Cli.json" /type:"solutionpackagers" /profile:"Extract-Unmanaged" /version:"7.1.1"
+exit
