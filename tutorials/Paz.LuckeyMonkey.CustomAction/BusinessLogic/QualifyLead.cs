d7f4c11dfca96112d341d046ec3bc02b3fea23de
PhuocLe <vanphuoc@gmail.com>
2019-03-30 13:46:45 +0700

    Finished test: Paz.LuckeyMonkey.CustomAction

---
+++ tutorials/Paz.LuckeyMonkey.CustomAction/BusinessLogic/QualifyLead.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Paz.LuckeyMonkey.Shared;
+using Paz.LuckeyMonkey.Shared.Entities;
+using Paz.LuckeyMonkey.Shared.Entities.LeadOptionSets;
+using System;
+using System.Linq;
+
+namespace Paz.LuckeyMonkey.CustomAction.BusinessLogic
+{
+    public class InputQualifyLead
+    {
+        public string LeadId { get; set; }
+    }
+
+    public class OutputQualifyLead
+    {
+        public string AccountId { get; set; }
+        public bool Status { get; set; }
+    }
+    public class QualifyLead
+    {
+        internal string Do(IPluginExecutionContext context, IOrganizationService service, ITracingService tracing, string jsonInput)
+        {
+            //Parse jsonInput to strong type InputQualifyLead
+            var input = SimpleJson.DeserializeObject<InputQualifyLead>(jsonInput);
+            //Call request QualifyLead
+            var request = new QualifyLeadRequest()
+            {
+                CreateAccount = true,
+                CreateOpportunity = false,
+                CreateContact = true,
+                LeadId = new EntityReference(Lead.EntityLogicalName, Guid.Parse(input.LeadId)),
+                Status = new OptionSetValue((int)StatusCode.Qualified)
+            };
+            var response = (QualifyLeadResponse)service.Execute(request);
+            var account = response.CreatedEntities.FirstOrDefault(r => r.LogicalName == Account.EntityLogicalName);
+            //Prepare output and Serialize to jsonOutput
+            var output = new OutputQualifyLead
+            {
+                Status = account != null,
+                AccountId = account?.Id.ToString()
+            };
+            return SimpleJson.SerializeObject(output);
+        }
+    }
+}
