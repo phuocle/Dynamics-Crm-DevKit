b39fe0b78915569da31db118c30395c9ef2e0c66
PhuocLe <vanphuoc@gmail.com>
2019-10-14 12:07:50 +0700

    Finished TaskCoverageToXml

---
+++ v2/DynamicsCrm.DevKit.Tool/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using CmdLine;
+using DynamicsCrm.DevKit.Tool.Commands;
+
+namespace DynamicsCrm.DevKit.Tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var type = CommandLine.Tokenize().Where(x => x.Command == "type").FirstOrDefault();
+            if (type == null)
+                throw new System.Exception("Not found /type switch");
+            switch (type.Value)
+            {
+                case "CoverageToXml":
+                    TaskCoverageToXml.Run();
+                    break;
+            }
+        }
+    }
+}
---
+++ v2/DynamicsCrm.DevKit.Tool/Utility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace DynamicsCrm.DevKit.Tool
+{
+    class Utility
+    {
+        public static string[] ParseArguments(string commandLine)
+        {
+            char[] parmChars = commandLine.ToCharArray();
+            bool inQuote = false;
+            for (int index = 0; index < parmChars.Length; index++)
+            {
+                if (parmChars[index] == '"')
+                    inQuote = !inQuote;
+                if (!inQuote && parmChars[index] == ' ')
+                    parmChars[index] = '\n';
+            }
+            return (new string(parmChars)).Split('\n');
+        }
+
+        public static string GetArgumentValue(string[] arguments, string argument)
+        {
+            var value = arguments.Where(x => x.StartsWith(argument)).FirstOrDefault();
+            if (value == null)
+                throw new Exception($"Cannot read value of: {argument}");
+            value = value.Substring(argument.Length);
+            if (value.StartsWith("\""))
+                value = value.Substring(1, value.Length - 2);
+            return value;
+        }
+    }
+}
