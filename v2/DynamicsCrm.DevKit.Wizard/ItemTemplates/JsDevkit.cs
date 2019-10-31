243571cbee27570bba02a3dedb3303842a404259
PhuocLe <vanphuoc@gmail.com>
2019-10-31 14:35:07 +0700

    Added project item template: Devkit.js

---
+++ v2/DynamicsCrm.DevKit.Wizard/ItemTemplates/JsDevkit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+
+namespace DynamicsCrm.DevKit.Wizard.ItemTemplates
+{
+    public class JsDevkit : IWizard
+    {
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+        }
+
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return true;
+        }
+    }
+}
