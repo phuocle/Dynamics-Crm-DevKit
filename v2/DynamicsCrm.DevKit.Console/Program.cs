72605e7b713e9dc841bd827b4956d3377d29580d
PhuocLe <vanphuoc@gmail.com>
2020-02-11 14:15:14 +0700

    Update and ready deploy version: 2.2.29

--- v2/DynamicsCrm.DevKit.Console/Program.cs
+++ v2/DynamicsCrm.DevKit.Console/Program.cs
@@ -1,11 +1,25 @@
-﻿namespace DynamicsCrm.DevKit.Console
+﻿using System;
+using System.IO;
+using NUglify;
+
+namespace DynamicsCrm.DevKit.Console
 {
     class Program
     {
         static void Main(string[] args)
         {
             //XrmMock.Generator_xrm_mock_d_ts();
-            XrmEnum.Generator();
+            //XrmEnum.Generator();
+            DevKitJsMin();
+        }
+
+        private static void DevKitJsMin()
+        {
+            var file = @"C:\src\github\phuocle\Dynamics-Crm-DevKit\v2\DynamicsCrm.DevKit.Resources\devkit.365.js";
+            var devkitCode = File.ReadAllText(file);
+            var devkitCodeMin = Uglify.Js(devkitCode).Code;
+            var output = @"C:\src\github\phuocle\Dynamics-Crm-DevKit\v2\ProjectTemplates\CSharp\05.WebResourceProjectTemplate\devkit.js";
+            File.WriteAllText(output, devkitCodeMin);
         }
     }
 }
