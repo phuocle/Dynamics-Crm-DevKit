a4d10da017250ff2152bfa299ecc2f610cddadc4
PhuocLe <vanphuoc@gmail.com>
2020-02-16 22:09:31 +0700

    Re-testing ...

--- test/Abc.LuckyStar/Abc.LuckyStar.WebResource/deploy.debug.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-for /f "delims=" %%d in ('dir /a:d /o:-n /s ..\packages\DynamicsCrm.DevKit.Cli.* /b') do (
-    set DynamicsCrmDevKitCli=%%d
-    goto break
-)
-:break
-set CrmConnection="AuthType=Office365;Url=https://orgcaffa69c.crm5.dynamics.com;Username=dev@pldevkit.onmicrosoft.com;Password=lXxH8LKZVyltmKMdhymaWcu5ooBpRRdWeJEScXnSFmM=;"
-"%DynamicsCrmDevKitCli%\tools\DynamicsCrm.DevKit.Cli.exe" /conn:%CrmConnection% /json:"..\DynamicsCrm.DevKit.Cli.json" /type:"webresources" /profile:"DEBUG"
-exit
