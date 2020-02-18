5ff1cbd5fe42e347e2bce00aea499f612c13e56e
PhuocLe <vanphuoc@gmail.com>
2020-02-18 22:22:06 +0700

    Tested !!!

--- test/Abc.LuckyStar/Abc.LuckyStar.Shared/Lib/Extension.cs
+++ test/Abc.LuckyStar/Abc.LuckyStar.Shared/Lib/Extension.cs
@@ -54,6 +54,23 @@ namespace Microsoft.Xrm.Sdk
             return lists;
         }
 
+        public static void DebugMessage(this ITracingService tracingService, string format, params object[] args)
+        { 
+#if DEBUG
+            tracingService.LogMessage(format, args);
+#else
+            { }
+#endif
+        }
+
+        public static void LogMessage(this ITracingService tracingService, string format, params object[] args)
+        {
+            string message = (args == null || args.Length == 0)
+                ? format
+                : String.Format(format, args);
+            tracingService.Trace("{0}: {1}", DateTime.Now.ToString("o"), message);
+        }
+
         private static string CreateXml(string xml, string cookie, int page, int count)
         {
             StringReader stringReader = new StringReader(xml);
