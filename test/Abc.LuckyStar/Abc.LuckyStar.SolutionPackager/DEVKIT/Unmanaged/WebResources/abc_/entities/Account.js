07bc57cb516aee8f280981c446eb12bbee2b2610
PhuocLe <vanphuoc@gmail.com>
2020-02-18 22:51:01 +0700

    Tested !!!

---
+++ test/Abc.LuckyStar/Abc.LuckyStar.SolutionPackager/DEVKIT/Unmanaged/WebResources/abc_/entities/Account.js
@@ -0,0 +1,28 @@
+﻿//@ts-check
+///<reference path="Account.d.ts" />
+var formAccount = (function () {
+    function onLoad(executionContext) {
+        var form = new LuckyStar.FormAccount(executionContext);
+        if (form.FormType === OptionSet.FormType.Create) {
+
+        }
+        var account = new LuckyStar.AccountApi();
+        account.AccountCategoryCode.Value = OptionSet.Account.AccountCategoryCode.Preferred_Customer;
+	}
+	function onSave(executionContext) {
+	}
+	return {
+		OnLoad: onLoad,
+		OnSave: onSave
+	};
+})();
+var formAccount_Quick_Create = (function () {
+	function onLoad(executionContext) {
+	}
+	function onSave(executionContext) {
+	}
+	return {
+		OnLoad: onLoad,
+		OnSave: onSave
+	};
+})();
