ba4f8ea21e7ddbdab2b03c8f9047de15e9549db1
PhuocLe <vanphuoc@gmail.com>
2020-10-06 10:18:11 +0700

    Testing ...

---
+++ test/v.2.10.31/Abc.LuckyStar/Abc.LuckyStar.Console.2/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+
+namespace Abc.LuckyStar.Console._2
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var UserId = ((WhoAmIResponse)AppSettings.Service.Execute(new WhoAmIRequest())).UserId;
+            System.Console.WriteLine(UserId);
+            System.Console.ReadKey();
+        }
+    }
+}
