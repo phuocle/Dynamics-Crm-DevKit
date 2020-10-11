df1f8ecb2f8f8de7983e5856c815f8a2993cdc9b
PhuocLe <vanphuoc@gmail.com>
2020-10-11 21:30:32 +0700

    Testing ...

---
+++ test/v.2.10.31/Abc.LuckyStar/Abc.LuckyStar.Plugin.Account.Test/PostAccountDeleteAsynchronousTest.cs
@@ -0,0 +1,126 @@
+﻿using FakeXrmEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Reflection;
+using Abc.LuckyStar.Shared;
+using Abc.LuckyStar.ProxyTypes;
+
+namespace Abc.LuckyStar.PluginAccount.Test
+{
+    [TestClass]
+    public class PostAccountDeleteAsynchronousTest
+    {
+        public static XrmFakedContext Context { get; set; }
+        public static XrmFakedPluginExecutionContext PluginContext { get; set; }
+
+        [ClassInitialize()]
+        public static void ClassInit(TestContext context)
+        {
+            Context = new XrmFakedContext();
+            Context.ProxyTypesAssembly = Assembly.GetAssembly(typeof(ProxyTypesAssembly));
+            PluginContext = Context.GetDefaultPluginContext();
+            PluginContext.PrimaryEntityName = "account";
+            PluginContext.MessageName = "Delete";
+            PluginContext.Stage = (int)StageEnum.PostOperation;
+            PluginContext.Mode = (int)ExecutionModeEnum.Asynchronous;
+            PluginContext.InputParameters["Target"] = null;
+        }
+
+        /*
+        [TestMethod]
+        public void _00_UnsecureString_And_SecureString()
+        {
+            var target = new Entity("account")
+            {
+                ["accountid"] = Guid.NewGuid()
+            };
+            PluginContext.InputParameters["Target"] = target;
+            var unsecureString = "UnsecureString";
+            var secureString = "SecureString";
+            Context.ExecutePluginWithConfigurations<PostAccountDeleteAsynchronous>(PluginContext, unsecureString, secureString);
+            Assert.IsTrue(target != null);
+        }
+        */
+
+        [TestMethod]
+        public void _01_Stage_Does_Not_Equals_PostOperation()
+        {
+            var context = new XrmFakedContext();
+            var plugin = context.GetDefaultPluginContext();
+            plugin.Stage = -1;
+            Assert.ThrowsException<InvalidPluginExecutionException>(() =>
+            {
+                context.ExecutePluginWith<PostAccountDeleteAsynchronous>(plugin);
+            }, "Stage does not equals PostOperation");
+        }
+
+        [TestMethod]
+        public void _02_PrimaryEntityName_Does_Not_Equals_account()
+        {
+            var context = new XrmFakedContext();
+            var plugin = context.GetDefaultPluginContext();
+            plugin.Stage = (int)StageEnum.PostOperation;
+            plugin.PrimaryEntityName = "abcd";
+            Assert.ThrowsException<InvalidPluginExecutionException>(() =>
+            {
+                context.ExecutePluginWith<PostAccountDeleteAsynchronous>(plugin);
+            }, "PrimaryEntityName does not equals account");
+        }
+
+        [TestMethod]
+        public void _03_MessageName_Does_Not_Equals_Delete()
+        {
+            var context = new XrmFakedContext();
+            var plugin = context.GetDefaultPluginContext();
+            plugin.Stage = (int)StageEnum.PostOperation;
+            plugin.PrimaryEntityName = "account";
+            plugin.MessageName = "abcd";
+            Assert.ThrowsException<InvalidPluginExecutionException>(() =>
+            {
+                context.ExecutePluginWith<PostAccountDeleteAsynchronous>(plugin);
+            }, "MessageName does not equals Delete");
+        }
+
+        [TestMethod]
+        public void _04_Mode_Does_Not_Equals_Asynchronous()
+        {
+            var context = new XrmFakedContext();
+            var plugin = context.GetDefaultPluginContext();
+            plugin.Stage = (int)StageEnum.PostOperation;
+            plugin.PrimaryEntityName = "account";
+            plugin.MessageName = "Delete";
+            plugin.Mode = -1;
+            Assert.ThrowsException<InvalidPluginExecutionException>(() =>
+            {
+                context.ExecutePluginWith<PostAccountDeleteAsynchronous>(plugin);
+            }, "Execution does not equals Asynchronous");
+        }
+
+        /*
+        [TestMethod]
+        public void _05_CrmPluginRegistration_Check()
+        {
+            var @class = new PostAccountDeleteAsynchronous();
+            foreach (var attribute in Attribute.GetCustomAttributes(@class.GetType()))
+            {
+                if (attribute.GetType().Equals(typeof(CrmPluginRegistrationAttribute)))
+                {
+                    var check = attribute as CrmPluginRegistrationAttribute;
+                    Assert.IsNotNull(check.Image1Attributes);
+                    Assert.IsNotNull(check.Image1Name);
+                    Assert.IsNotNull(check.Image1Type);
+                }
+                else
+                    Assert.Fail();
+            }
+        }
+        */
+
+        [TestMethod]
+        public void _06()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
