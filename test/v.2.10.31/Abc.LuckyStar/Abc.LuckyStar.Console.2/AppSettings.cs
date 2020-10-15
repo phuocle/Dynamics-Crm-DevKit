2b7065b1653216ae004b3db8b0fbae68b96e2b1f
PhuocLe <vanphuoc@gmail.com>
2020-10-15 18:11:48 +0700

    Testing ...

--- test/v.2.10.31/Abc.LuckyStar/Abc.LuckyStar.Console.2/AppSettings.cs
+++ test/v.2.10.31/Abc.LuckyStar/Abc.LuckyStar.Console.2/AppSettings.cs
@@ -1,54 +1,26 @@
 ﻿using System;
-using System.Configuration;
-using System.Net;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Tooling.Connector;
-using PowerApps.Samples.LoginUX;
 
 namespace Abc.LuckyStar.Console._2
 {
     public static class AppSettings
     {
         public static IOrganizationService Service = InitializeIOrganizationService();
-        private static string AuthType { get { return ConfigurationManager.AppSettings["AuthType"]; } }
-        private static string Url { get { return ConfigurationManager.AppSettings["Url"]; } }
-        private static string Username { get { return ConfigurationManager.AppSettings["Username"]; } }
-        private static string Password { get { return ConfigurationManager.AppSettings["Password"]; } }
-        private static string ConnectionString
-        {
-            get
-            {
-                return $"AuthType={AuthType};Url={Url};Username={Username};Password={Password};";
-            }
-        }
 
         private static void LoginFrm_ConnectionToCrmCompleted(object sender, EventArgs e)
         {
-            if (sender is ExampleLoginForm)
+            if (sender is LoginForm)
             {
-                ((ExampleLoginForm)sender).Close();
+                ((LoginForm)sender).Close();
             }
         }
 
         static IOrganizationService InitializeIOrganizationService()
         {
-            //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-            //var crmServiceClient = new CrmServiceClient(ConnectionString);
-            //if (crmServiceClient.OrganizationServiceProxy != null)
-            //    return (IOrganizationService)crmServiceClient.OrganizationServiceProxy;
-            //else if (crmServiceClient.OrganizationWebProxyClient != null)
-            //    return (IOrganizationService)crmServiceClient.OrganizationWebProxyClient;
-            //else
-            //    throw new Exception("Service NULL");
-
-
-            ExampleLoginForm loginFrm = new ExampleLoginForm();
-            // Login process is Async, thus we need to detect when login is completed and close the form.
+            var loginFrm = new LoginForm();
             loginFrm.ConnectionToCrmCompleted += LoginFrm_ConnectionToCrmCompleted;
-            // Show the dialog here.
             loginFrm.ShowDialog();
-
-            // If the login process completed, assign the connected service to the CRMServiceClient var
             if (loginFrm.CrmConnectionMgr != null && loginFrm.CrmConnectionMgr.CrmSvc != null && loginFrm.CrmConnectionMgr.CrmSvc.IsReady)
             {
                 CrmServiceClient crmServiceClient = loginFrm.CrmConnectionMgr.CrmSvc;
--- test/v.2.10.31/Abc.LuckyStar/Abc.LuckyStar.Console.2/Login/LoginForm.xaml.cs
+++ test/v.2.10.31/Abc.LuckyStar/Abc.LuckyStar.Console.2/Login/LoginForm.xaml.cs
@@ -4,12 +4,12 @@ using System;
 using System.Windows;
 using System.Windows.Threading;
 
-namespace PowerApps.Samples.LoginUX
+namespace Abc.LuckyStar.Console._2
 {
     /// <summary>
     /// Interaction logic for ExampleLoginForm.xaml
     /// </summary>
-    public partial class ExampleLoginForm : Window
+    public partial class LoginForm : Window
     {
         #region Vars
         /// <summary>
@@ -17,35 +17,35 @@ namespace PowerApps.Samples.LoginUX
         /// </summary>
         private CrmServiceClient CrmSvc = null;
         /// <summary>
-        /// Bool flag to determine if there is a connection 
+        /// Bool flag to determine if there is a connection
         /// </summary>
         private bool bIsConnectedComplete = false;
         /// <summary>
-        /// CRM Connection Manager component. 
+        /// CRM Connection Manager component.
         /// </summary>
         private CrmConnectionManager mgr = null;
         /// <summary>
-        ///  This is used to allow the UI to reset w/out closing 
+        ///  This is used to allow the UI to reset w/out closing
         /// </summary>
         private bool resetUiFlag = false;
         #endregion
 
         #region Properties
         /// <summary>
-        /// CRM Connection Manager 
+        /// CRM Connection Manager
         /// </summary>
         public CrmConnectionManager CrmConnectionMgr { get { return mgr; } }
         #endregion
 
         #region Event
         /// <summary>
-        /// Raised when a connection to CRM has completed. 
+        /// Raised when a connection to CRM has completed.
         /// </summary>
         public event EventHandler ConnectionToCrmCompleted;
         #endregion
 
 
-        public ExampleLoginForm()
+        public LoginForm()
         {
             InitializeComponent();
             //// Should be used for testing only.
@@ -57,63 +57,63 @@ namespace PowerApps.Samples.LoginUX
         }
 
         /// <summary>
-        /// Raised when the window loads for the first time. 
+        /// Raised when the window loads for the first time.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             /*
-				This is the setup process for the login control, 
-				The login control uses a class called CrmConnectionManager to manage the interaction with CRM, this class and also be queried as later points for information about the current connection. 
+				This is the setup process for the login control,
+				The login control uses a class called CrmConnectionManager to manage the interaction with CRM, this class and also be queried as later points for information about the current connection.
 				In this case, the login control is referred to as CrmLoginCtrl
 			 */
 
-            // Set off flag. 
+            // Set off flag.
             bIsConnectedComplete = false;
 
-            // Init the CRM Connection manager.. 
+            // Init the CRM Connection manager..
             mgr = new CrmConnectionManager();
             // Pass a reference to the current UI or container control,  this is used to synchronize UI threads In the login control
             mgr.ParentControl = CrmLoginCtrl;
             // if you are using an unmanaged client, excel for example, and need to store the config in the users local directory
-            // set this option to true. 
+            // set this option to true.
             mgr.UseUserLocalDirectoryForConfigStore = true;
 
-            // Client ID to use for oAuth.  ID: 51f81489-12ee-4a9e-aaae-a2591f45987d is a sample ID that should be replaced by a ID created for your use. 
+            // Client ID to use for oAuth.  ID: 51f81489-12ee-4a9e-aaae-a2591f45987d is a sample ID that should be replaced by a ID created for your use.
             mgr.ClientId = "51f81489-12ee-4a9e-aaae-a2591f45987d";
             // Redirect URI required for oAuth.
             mgr.RedirectUri = new Uri("app://58145B91-0C36-4500-8554-080854F2AC97");
 
-            // if you are using an unmanaged client,  you need to provide the name of an exe to use to create app config key's for. 
+            // if you are using an unmanaged client,  you need to provide the name of an exe to use to create app config key's for.
             //mgr.HostApplicatioNameOveride = "MyExecName.exe";
-            // CrmLoginCtrl is the Login control,  this sets the CrmConnection Manager into it. 
+            // CrmLoginCtrl is the Login control,  this sets the CrmConnection Manager into it.
             CrmLoginCtrl.SetGlobalStoreAccess(mgr);
             // There are several modes to the login control UI
             CrmLoginCtrl.SetControlMode(ServerLoginConfigCtrlMode.FullLoginPanel);
-            // this wires an event that is raised when the login button is pressed. 
+            // this wires an event that is raised when the login button is pressed.
             CrmLoginCtrl.ConnectionCheckBegining += new EventHandler(CrmLoginCtrl_ConnectionCheckBegining);
-            // this wires an event that is raised when an error in the connect process occurs. 
+            // this wires an event that is raised when an error in the connect process occurs.
             CrmLoginCtrl.ConnectErrorEvent += new EventHandler<ConnectErrorEventArgs>(CrmLoginCtrl_ConnectErrorEvent);
-            // this wires an event that is raised when a status event is returned. 
+            // this wires an event that is raised when a status event is returned.
             CrmLoginCtrl.ConnectionStatusEvent += new EventHandler<ConnectStatusEventArgs>(CrmLoginCtrl_ConnectionStatusEvent);
-            // this wires an event that is raised when the user clicks the cancel button. 
+            // this wires an event that is raised when the user clicks the cancel button.
             CrmLoginCtrl.UserCancelClicked += new EventHandler(CrmLoginCtrl_UserCancelClicked);
-            // Check to see if its possible to do an Auto Login 
+            // Check to see if its possible to do an Auto Login
             if (!mgr.RequireUserLogin())
             {
                 if (MessageBox.Show("Credentials already saved in configuration\nChoose Yes to Auto Login or No to Reset Credentials", "Auto Login", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
                 {
-                    // If RequireUserLogin is false, it means that there has been a successful login here before and the credentials are cached. 
+                    // If RequireUserLogin is false, it means that there has been a successful login here before and the credentials are cached.
                     CrmLoginCtrl.IsEnabled = false;
                     // When running an auto login,  you need to wire and listen to the events from the connection manager.
-                    // Run Auto User Login process, Wire events. 
+                    // Run Auto User Login process, Wire events.
                     mgr.ServerConnectionStatusUpdate += new EventHandler<ServerConnectStatusEventArgs>(mgr_ServerConnectionStatusUpdate);
                     mgr.ConnectionCheckComplete += new EventHandler<ServerConnectStatusEventArgs>(mgr_ConnectionCheckComplete);
-                    // Start the connection process. 
+                    // Start the connection process.
                     mgr.ConnectToServerCheck();
 
-                    // Show the message grid. 
+                    // Show the message grid.
                     CrmLoginCtrl.ShowMessageGrid();
                 }
             }
@@ -122,14 +122,14 @@ namespace PowerApps.Samples.LoginUX
         #region Events
 
         /// <summary>
-        /// Updates from the Auto Login process. 
+        /// Updates from the Auto Login process.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void mgr_ServerConnectionStatusUpdate(object sender, ServerConnectStatusEventArgs e)
         {
-            // The Status event will contain information about the current login process,  if Connected is false, then there is not yet a connection. 
-            // Set the updated status of the loading process. 
+            // The Status event will contain information about the current login process,  if Connected is false, then there is not yet a connection.
+            // Set the updated status of the loading process.
             Dispatcher.Invoke(DispatcherPriority.Normal,
                                new System.Action(() =>
                                {
@@ -146,14 +146,14 @@ namespace PowerApps.Samples.LoginUX
         /// <param name="e"></param>
         private void mgr_ConnectionCheckComplete(object sender, ServerConnectStatusEventArgs e)
         {
-            // The Status event will contain information about the current login process,  if Connected is false, then there is not yet a connection. 
-            // Unwire events that we are not using anymore, this prevents issues if the user uses the control after a failed login. 
+            // The Status event will contain information about the current login process,  if Connected is false, then there is not yet a connection.
+            // Unwire events that we are not using anymore, this prevents issues if the user uses the control after a failed login.
             ((CrmConnectionManager)sender).ConnectionCheckComplete -= mgr_ConnectionCheckComplete;
             ((CrmConnectionManager)sender).ServerConnectionStatusUpdate -= mgr_ServerConnectionStatusUpdate;
 
             if (!e.Connected)
             {
-                // if its not connected pop the login screen here. 
+                // if its not connected pop the login screen here.
                 if (e.MultiOrgsFound)
                     MessageBox.Show("Unable to Login to CRM using cached credentials. Org Not found", "Login Failure");
                 else
@@ -161,7 +161,7 @@ namespace PowerApps.Samples.LoginUX
 
                 resetUiFlag = true;
                 CrmLoginCtrl.GoBackToLogin();
-                // Bad Login Get back on the UI. 
+                // Bad Login Get back on the UI.
                 Dispatcher.Invoke(DispatcherPriority.Normal,
                        new System.Action(() =>
                        {
@@ -174,7 +174,7 @@ namespace PowerApps.Samples.LoginUX
             }
             else
             {
-                // Good Login Get back on the UI 
+                // Good Login Get back on the UI
                 if (e.Connected && !bIsConnectedComplete)
                     ProcessSuccess();
             }
@@ -182,7 +182,7 @@ namespace PowerApps.Samples.LoginUX
         }
 
         /// <summary>
-        ///  Login control connect check starting. 
+        ///  Login control connect check starting.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -199,20 +199,20 @@ namespace PowerApps.Samples.LoginUX
         }
 
         /// <summary>
-        /// Login control connect check status event. 
+        /// Login control connect check status event.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void CrmLoginCtrl_ConnectionStatusEvent(object sender, ConnectStatusEventArgs e)
         {
-            //Here we are using the bIsConnectedComplete bool to check to make sure we only process this call once. 
+            //Here we are using the bIsConnectedComplete bool to check to make sure we only process this call once.
             if (e.ConnectSucceeded && !bIsConnectedComplete)
                 ProcessSuccess();
 
         }
 
         /// <summary>
-        /// Login control Error event. 
+        /// Login control Error event.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -222,7 +222,7 @@ namespace PowerApps.Samples.LoginUX
         }
 
         /// <summary>
-        /// Login Control Cancel event raised. 
+        /// Login Control Cancel event raised.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -251,7 +251,7 @@ namespace PowerApps.Samples.LoginUX
                }
                 ));
 
-            // Notify Caller that we are done with success. 
+            // Notify Caller that we are done with success.
             if (ConnectionToCrmCompleted != null)
                 ConnectionToCrmCompleted(this, null);
 
@@ -295,7 +295,7 @@ namespace PowerApps.Samples.LoginUX
           <add name ="fileListener"/>
         </listeners>
       </source>
-      
+
     <!-- WCF DEBUG SOURCES -->
       <source name="System.IdentityModel" switchName="System.IdentityModel">
         <listeners>
@@ -318,7 +318,7 @@ namespace PowerApps.Samples.LoginUX
       <!-- END WCF DEBUG SOURCES -->
     </sources>
     <switches>
-      <!-- 
+      <!--
             Possible values for switches: Off, Error, Warining, Info, Verbose
                 Verbose:    includes Error, Warning, Info, Trace levels
                 Info:       includes Error, Warning, Info levels
@@ -331,7 +331,7 @@ namespace PowerApps.Samples.LoginUX
       <add name="System.IdentityModel" value="Verbose"/>
       <add name="System.ServiceModel.MessageLogging" value="Verbose"/>
       <add name="System.ServiceModel" value="Error, ActivityTracing"/>
-      
+
     </switches>
     <sharedListeners>
       <add name="fileListener" type="System.Diagnostics.TextWriterTraceListener" initializeData="LoginControlTesterLog.txt"/>
