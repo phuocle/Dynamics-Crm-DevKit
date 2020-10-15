f22db870660816402489adbaa520b638edea501d
PhuocLe <vanphuoc@gmail.com>
2020-10-15 18:01:37 +0700

    Testing ...

--- test/v.2.10.31/Abc.LuckyStar/Abc.LuckyStar.Console.2/AppSettings.cs
+++ test/v.2.10.31/Abc.LuckyStar/Abc.LuckyStar.Console.2/AppSettings.cs
@@ -3,6 +3,7 @@ using System.Configuration;
 using System.Net;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Tooling.Connector;
+using PowerApps.Samples.LoginUX;
 
 namespace Abc.LuckyStar.Console._2
 {
@@ -20,16 +21,45 @@ namespace Abc.LuckyStar.Console._2
                 return $"AuthType={AuthType};Url={Url};Username={Username};Password={Password};";
             }
         }
+
+        private static void LoginFrm_ConnectionToCrmCompleted(object sender, EventArgs e)
+        {
+            if (sender is ExampleLoginForm)
+            {
+                ((ExampleLoginForm)sender).Close();
+            }
+        }
+
         static IOrganizationService InitializeIOrganizationService()
         {
-            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-            var crmServiceClient = new CrmServiceClient(ConnectionString);
-            if (crmServiceClient.OrganizationServiceProxy != null)
-                return (IOrganizationService)crmServiceClient.OrganizationServiceProxy;
-            else if (crmServiceClient.OrganizationWebProxyClient != null)
-                return (IOrganizationService)crmServiceClient.OrganizationWebProxyClient;
-            else
-                throw new Exception("Service NULL");
+            //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            //var crmServiceClient = new CrmServiceClient(ConnectionString);
+            //if (crmServiceClient.OrganizationServiceProxy != null)
+            //    return (IOrganizationService)crmServiceClient.OrganizationServiceProxy;
+            //else if (crmServiceClient.OrganizationWebProxyClient != null)
+            //    return (IOrganizationService)crmServiceClient.OrganizationWebProxyClient;
+            //else
+            //    throw new Exception("Service NULL");
+
+
+            ExampleLoginForm loginFrm = new ExampleLoginForm();
+            // Login process is Async, thus we need to detect when login is completed and close the form.
+            loginFrm.ConnectionToCrmCompleted += LoginFrm_ConnectionToCrmCompleted;
+            // Show the dialog here.
+            loginFrm.ShowDialog();
+
+            // If the login process completed, assign the connected service to the CRMServiceClient var
+            if (loginFrm.CrmConnectionMgr != null && loginFrm.CrmConnectionMgr.CrmSvc != null && loginFrm.CrmConnectionMgr.CrmSvc.IsReady)
+            {
+                CrmServiceClient crmServiceClient = loginFrm.CrmConnectionMgr.CrmSvc;
+                if (crmServiceClient.OrganizationServiceProxy != null)
+                    return (IOrganizationService)crmServiceClient.OrganizationServiceProxy;
+                else if (crmServiceClient.OrganizationWebProxyClient != null)
+                    return (IOrganizationService)crmServiceClient.OrganizationWebProxyClient;
+                else
+                    throw new Exception("Service NULL");
+            }
+            throw new Exception("Service NULL");
         }
     }
 }
--- test/v.2.10.31/Abc.LuckyStar/Abc.LuckyStar.Console.2/Program.cs
+++ test/v.2.10.31/Abc.LuckyStar/Abc.LuckyStar.Console.2/Program.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.Crm.Sdk.Messages;
+using System;
 
 namespace Abc.LuckyStar.Console._2
 {
     public class Program
     {
+        [STAThread]
         static void Main()
         {
             var UserId = ((WhoAmIResponse)AppSettings.Service.Execute(new WhoAmIRequest())).UserId;
