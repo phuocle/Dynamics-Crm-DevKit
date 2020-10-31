b52748847f2ba54c085d3de2cbb542ffa615d4e9
PhuocLe <vanphuoc@gmail.com>
2020-10-31 12:05:50 +0700

    !!!!!! DELETED !!!!!!!

--- test/Abc.LuckyStar/Abc.LuckyStar.Workflow.Account/deploy.debug.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-for /f "delims=" %%d in ('dir /a:d /o:-n /s ..\packages\DynamicsCrm.DevKit.Cli.* /b') do (
-    set DynamicsCrmDevKitCli=%%d
-    goto break
-)
-:break
-set CrmConnection="AuthType=Office365;Url=https://dev-devkit.crm5.dynamics.com;Username=dev@pldevkit.onmicrosoft.com;Password=b6+abJ1xam0vxgUEg98P7usY3mJ5IMCVuzElnwzAymo=;"
-"%DynamicsCrmDevKitCli%\tools\DynamicsCrm.DevKit.Cli.exe" /conn:%CrmConnection% /json:"..\DynamicsCrm.DevKit.Cli.json" /type:"workflows" /profile:"DEBUG"
-exit
