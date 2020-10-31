c17bcd1c69e20c87175ba61434fda9aeb01b90fd
PhuocLe <vanphuoc@gmail.com>
2020-10-31 11:45:02 +0700

    Testing ...

---
+++ test/v.2.10.31/Abc.LuckyStar2/Abc.LuckyStar2.Shared/Entities/SolutionComponent.cs
@@ -0,0 +1,16 @@
+﻿namespace Abc.LuckyStar2.Shared.Entities
+{
+    public partial class SolutionComponent
+    {
+        #region --- PROPERTIES ---
+
+        //public DateTime? DateTime { get { return GetAliasedValue<DateTime?>("c.birthdate"); } }
+
+        #endregion
+
+        #region --- STATIC METHODS ---
+
+
+        #endregion
+    }
+}
---
+++ test/v.2.10.31/Abc.LuckyStar2/Abc.LuckyStar2.Shared/Entities/devkit_CustomActivity.cs
@@ -0,0 +1,16 @@
+﻿namespace Abc.LuckyStar2.Shared.Entities
+{
+    public partial class devkit_CustomActivity
+    {
+        #region --- PROPERTIES ---
+
+        //public DateTime? DateTime { get { return GetAliasedValue<DateTime?>("c.birthdate"); } }
+
+        #endregion
+
+        #region --- STATIC METHODS ---
+
+
+        #endregion
+    }
+}
