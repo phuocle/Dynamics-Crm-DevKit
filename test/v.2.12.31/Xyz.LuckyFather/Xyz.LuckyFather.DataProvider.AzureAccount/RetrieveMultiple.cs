ceed350b4c5b5342fc8ede7399c747c68fef994e
PhuocLe <vanphuoc@gmail.com>
2021-04-08 18:14:48 +0700

    Continue working ...

---
+++ test/v.2.12.31/Xyz.LuckyFather/Xyz.LuckyFather.DataProvider.AzureAccount/RetrieveMultiple.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Extensions;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using Xyz.LuckyFather.Shared;
+
+namespace Xyz.LuckyFather.DataProvider.AzureAccount
+{
+    [CrmPluginRegistration("Xyz.LuckyFather.DataProvider.AzureAccount.RetrieveMultiple", VirtualTablePlugin.RetrieveMultiple)]
+    public class RetrieveMultiple : IPlugin
+    {
+        /*
+          InputParameters:
+              Query                 Microsoft.Xrm.Sdk.Query.QueryBase - require
+              AppModuleId           System.Guid
+              IsAppModuleContext    System.Boolean
+           OutputParameters:
+              EntityCollection      Microsoft.Xrm.Sdk.EntityCollection - require
+        */
+
+        //private readonly string _unsecureString = null;
+        //private readonly string _secureString = null;
+
+        //public RetrieveMultiple(string unsecureString, string secureString)
+        //{
+        //    if (!string.IsNullOrWhiteSpace(unsecureString)) _unsecureString = unsecureString;
+        //    if (!string.IsNullOrWhiteSpace(secureString)) _secureString = secureString;
+        //}
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var service = serviceFactory.CreateOrganizationService(context.UserId);
+            var tracing = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            var retriever = serviceProvider.Get<IEntityDataSourceRetrieverService>();
+            var dataSource = retriever.RetrieveEntityDataSource();
+
+            //tracing.DebugMessage("Begin Data Provider: Xyz.LuckyFather.DataProvider.AzureAccount.RetrieveMultiple");
+            //tracing.DebugContext(context);
+
+            ExecutePlugin(context, serviceFactory, service, tracing, dataSource);
+
+            //tracing.DebugMessage("End Data Provider: Xyz.LuckyFather.DataProvider.AzureAccount.RetrieveMultiple");
+        }
+
+        private void ExecutePlugin(IPluginExecutionContext context, IOrganizationServiceFactory serviceFactory, IOrganizationService service, ITracingService tracing, Entity dataSource)
+        {
+            //Get Parameter from DataSource
+            //var ??? = dataSource.GetAttributeValue<string>("???");
+            //var ??? = dataSource.GetAttributeValue<int>("???");
+
+            var query = context.InputParameterOrDefault<QueryExpression>("Query");
+            var entities = new EntityCollection();
+
+            //YOUR CODE ...
+            entities.EntityName = "???";
+
+            context.OutputParameters["BusinessEntityCollection"] = entities;
+        }
+    }
+}
