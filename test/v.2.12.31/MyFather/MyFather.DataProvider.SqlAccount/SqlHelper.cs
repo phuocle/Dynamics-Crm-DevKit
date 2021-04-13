c79e4f5c258b6c9b601fce16af35643c144b507f
PhuocLe <vanphuoc@gmail.com>
2021-04-13 12:54:33 +0700

    Continue working ...

--- test/v.2.12.31/MyFather/MyFather.DataProvider.SqlAccount/SqlHelper.cs
+++ test/v.2.12.31/MyFather/MyFather.DataProvider.SqlAccount/SqlHelper.cs
@@ -5,6 +5,7 @@ using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Extensions;
 using Microsoft.Xrm.Sdk.Query;
 using MyFather.DataProvider.SqlAccount.Mappers;
+using MyFather.Shared.Entities;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -19,8 +20,8 @@ namespace MyFather.DataProvider.SqlAccount
         public static EntityCollection RetrieveMultipleToSql(IPluginExecutionContext context, IOrganizationService service, ITracingService tracing, Entity dataSource)
         {
             var collection = new EntityCollection();
-            var sqlConnectionString = dataSource.GetAttributeValue<string>("devkit_sqlconnectionstring");
-            if (sqlConnectionString == null) return collection;
+            var setting = new devkit_azuresql(dataSource);
+            if (setting.devkit_SqlConnectionString == null) return collection;
             var query = context.InputParameters["Query"];
             var fetchXml = string.Empty;
             if (query is QueryExpression qe)
@@ -55,24 +56,24 @@ namespace MyFather.DataProvider.SqlAccount
             sql = mapper.MapVirtualEntityAttributes(sql);
             if (page != -1 && count != -1)
             {
-                collection = GetEntitiesFromSql(context, mapper, sqlConnectionString, sql, count, page);
+                collection = GetEntitiesFromSql(context, mapper, setting.devkit_SqlConnectionString, sql, count, page);
             }
             else
             {
-                collection = GetEntitiesFromSql(context, mapper, sqlConnectionString, sql, -1, 1);
+                collection = GetEntitiesFromSql(context, mapper, setting.devkit_SqlConnectionString, sql, -1, 1);
             }
             return collection;
         }
 
         public static Entity RetrieveToSql(IPluginExecutionContext context, IOrganizationService service, ITracingService tracing, Entity dataSource)
         {
-            var sqlConnectionString = dataSource.GetAttributeValue<string>("devkit_sqlconnectionstring");
+            var setting = new devkit_azuresql(dataSource);
             var entity = new Entity(context.PrimaryEntityName, context.PrimaryEntityId);
             var mapper = new GenericMapper(context, service, tracing);
             string sql = $"SELECT * FROM {context.PrimaryEntityName} WITH(NOLOCK) WHERE {mapper.PrimaryEntityMetadata.PrimaryIdAttribute} = '{mapper.MapToVirtualEntityValue(mapper.PrimaryEntityMetadata.PrimaryIdAttribute, context.PrimaryEntityId)}'";
             sql = mapper.MapVirtualEntityAttributes(sql);
 
-            var entities = GetEntitiesFromSql(context, mapper, sqlConnectionString, sql, 1, 1);
+            var entities = GetEntitiesFromSql(context, mapper, setting.devkit_SqlConnectionString, sql, 1, 1);
             if (entities.Entities != null && entities.Entities.Count > 0)
             {
                 entity = entities.Entities[0];
@@ -85,12 +86,12 @@ namespace MyFather.DataProvider.SqlAccount
 
         public static void UpdateToSql(IPluginExecutionContext context, IOrganizationService service, ITracingService tracing, Entity dataSource)
         {
-            var sqlConnectionString = dataSource.GetAttributeValue<string>("devkit_sqlconnectionstring");
+            var setting = new devkit_azuresql(dataSource);
             var mapper = new GenericMapper(context, service, tracing);
             var mappings = mapper.GetCustomMappings();
             var entity = context.InputParameterOrDefault<Entity>("Target");
             string sql = $"UPDATE {mappings[context.PrimaryEntityName]} SET {{0}} WHERE {mappings[mapper.PrimaryEntityMetadata.PrimaryIdAttribute]} = '{context.PrimaryEntityId}'";
-            using (SqlConnection sqlConnection = new SqlConnection(sqlConnectionString))
+            using (SqlConnection sqlConnection = new SqlConnection(setting.devkit_SqlConnectionString))
             {
                 using(SqlCommand command = sqlConnection.CreateCommand())
                 {
@@ -98,8 +99,14 @@ namespace MyFather.DataProvider.SqlAccount
                     foreach (var attribute in entity.Attributes)
                     {
                         if (attribute.Key == mapper.PrimaryEntityMetadata.PrimaryIdAttribute) continue;
+                        if (mappings[attribute.Key] == setting.devkit_ExternalCreatedOnField || mappings[attribute.Key] == setting.devkit_ExternalModifiedOnField) continue;
                         command.Parameters.AddWithValue($"@{mappings[attribute.Key]}", GetValueOfAttribute(attribute.Value));
                         setList.Add($"{mappings[attribute.Key]}=@{mappings[attribute.Key]}");
+                    }
+                    if (setting.devkit_ExternalModifiedOnField != null)
+                    {
+                        setList.Add($"{setting.devkit_ExternalModifiedOnField}=@{setting.devkit_ExternalModifiedOnField}");
+                        command.Parameters.AddWithValue($"@{setting.devkit_ExternalModifiedOnField}", DateTime.UtcNow);
                     }
                     sql = string.Format(sql, string.Join(", ", setList));
                     command.CommandText = sql;
@@ -112,13 +119,13 @@ namespace MyFather.DataProvider.SqlAccount
 
         public static Guid CreateToSql(IPluginExecutionContext context, IOrganizationService service, ITracingService tracing, Entity dataSource)
         {
-            var sqlConnectionString = dataSource.GetAttributeValue<string>("devkit_sqlconnectionstring");
+            var setting = new devkit_azuresql(dataSource);
             var mapper = new GenericMapper(context, service, tracing);
             var mappings = mapper.GetCustomMappings();
             var entity = context.InputParameterOrDefault<Entity>("Target");
             string sql = $"INSERT INTO {mappings[context.PrimaryEntityName]}({{0}}) VALUES ({{1}})";
             var id = Guid.NewGuid();
-            using (SqlConnection sqlConnection = new SqlConnection(sqlConnectionString))
+            using (SqlConnection sqlConnection = new SqlConnection(setting.devkit_SqlConnectionString))
             {
                 using (SqlCommand command = sqlConnection.CreateCommand())
                 {
@@ -126,13 +133,29 @@ namespace MyFather.DataProvider.SqlAccount
                     List<string> values = new List<string>();
                     foreach (var attribute in entity.Attributes)
                     {
-                        command.Parameters.AddWithValue($"@{mappings[attribute.Key]}", GetValueOfAttribute(attribute.Value));
+                        if (mappings[attribute.Key] == setting.devkit_ExternalCreatedOnField || mappings[attribute.Key] == setting.devkit_ExternalModifiedOnField)
+                            continue;
                         columns.Add($"{mappings[attribute.Key]}");
                         values.Add($"@{mappings[attribute.Key]}");
+                        command.Parameters.AddWithValue($"@{mappings[attribute.Key]}", GetValueOfAttribute(attribute.Value));
                     }
+
                     columns.Add($"{mappings[mapper.PrimaryEntityMetadata.PrimaryIdAttribute]}");
                     values.Add($"@{mappings[mapper.PrimaryEntityMetadata.PrimaryIdAttribute]}");
                     command.Parameters.AddWithValue($"@{mappings[mapper.PrimaryEntityMetadata.PrimaryIdAttribute]}", id);
+
+                    if (setting.devkit_ExternalCreatedOnField != null)
+                    {
+                        columns.Add($"{setting.devkit_ExternalCreatedOnField}");
+                        values.Add($"@{setting.devkit_ExternalCreatedOnField}");
+                        command.Parameters.AddWithValue($"@{setting.devkit_ExternalCreatedOnField}", DateTime.UtcNow);
+                    }
+                    if (setting.devkit_ExternalModifiedOnField != null)
+                    {
+                        columns.Add($"{setting.devkit_ExternalModifiedOnField}");
+                        values.Add($"@{setting.devkit_ExternalModifiedOnField}");
+                        command.Parameters.AddWithValue($"@{setting.devkit_ExternalModifiedOnField}", DateTime.UtcNow);
+                    }
 
                     sql = string.Format(sql, string.Join(", ", columns), string.Join(", ", values));
                     command.CommandText = sql;
@@ -146,11 +169,11 @@ namespace MyFather.DataProvider.SqlAccount
 
         public static void DeleteToSql(IPluginExecutionContext context, IOrganizationService service, ITracingService tracing, Entity dataSource)
         {
-            var sqlConnectionString = dataSource.GetAttributeValue<string>("devkit_sqlconnectionstring");
+            var setting = new devkit_azuresql(dataSource);
             var mapper = new GenericMapper(context, service, tracing);
             var mappings = mapper.GetCustomMappings();
             string sql = $"DELETE {mappings[context.PrimaryEntityName]} WHERE {mappings[mapper.PrimaryEntityMetadata.PrimaryIdAttribute]} = '{context.PrimaryEntityId}'";
-            using (SqlConnection sqlConnection = new SqlConnection(sqlConnectionString))
+            using (SqlConnection sqlConnection = new SqlConnection(setting.devkit_SqlConnectionString))
             {
                 using (SqlCommand command = sqlConnection.CreateCommand())
                 {
---
+++ test/v.2.12.31/MyFather/MyFather.Shared/Entities/devkit_azuresql.cs
@@ -0,0 +1,17 @@
+﻿
+namespace MyFather.Shared.Entities
+{
+    public partial class devkit_azuresql
+    {
+        #region --- PROPERTIES ---
+
+        //public DateTime? DateTime { get { return GetAliasedValue<DateTime?>("c.birthdate"); } }
+
+        #endregion
+
+        #region --- STATIC METHODS ---
+
+
+        #endregion
+    }
+}
