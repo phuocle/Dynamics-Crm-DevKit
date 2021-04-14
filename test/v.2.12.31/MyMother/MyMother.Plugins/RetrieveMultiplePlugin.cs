5ed3bb0fd7d1bf4d1435ad8312136de578c6bcf4
PhuocLe <vanphuoc@gmail.com>
2021-04-14 13:31:49 +0700

    Continue working ...

---
+++ test/v.2.12.31/MyMother/MyMother.Plugins/RetrieveMultiplePlugin.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMother.Plugins
+{
+    public class RetrieveMultiplePlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
