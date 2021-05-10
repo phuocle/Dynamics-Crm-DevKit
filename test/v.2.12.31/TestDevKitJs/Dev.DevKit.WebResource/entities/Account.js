47612eace143859dcbd1c4cf4d69f02038239f80
PhuocLe <vanphuoc@gmail.com>
2021-05-10 14:01:59 +0700

    Continue working ...

--- test/v.2.12.31/TestDevKitJs/Dev.DevKit.WebResource/entities/Account.js
+++ test/v.2.12.31/TestDevKitJs/Dev.DevKit.WebResource/entities/Account.js
@@ -4,9 +4,16 @@
 var formAccount = (function () {
 	"use strict";
 	async function onLoad(executionContext) {
+		checkIsFormUpdate(executionContext);
 	}
 	async function onSave(executionContext) {
 	}
+	function checkIsFormUpdate(executionContext) {
+		var form = new DevKit.FormAccount(executionContext);
+		if (form.FormType === OptionSet.FormType.Update) {
+			form.Body.Name.Disabled = true;
+        }
+    }
 	return {
 		OnLoad: onLoad,
 		OnSave: onSave
