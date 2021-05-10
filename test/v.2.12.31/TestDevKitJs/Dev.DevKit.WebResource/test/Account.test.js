1cd9c69ef0dd82eb384dd7a9f8c814bf429175f2
PhuocLe <vanphuoc@gmail.com>
2021-05-10 10:01:33 +0700

    Continue working ...

---
+++ test/v.2.12.31/TestDevKitJs/Dev.DevKit.WebResource/test/Account.test.js
@@ -0,0 +1,13 @@
+﻿//@ts-check
+define(['xrm-mock', 'sinon'], function () {
+    var xrmMock = require('xrm-mock');
+    var sinon = require('sinon');
+    describe('Account.test.js Test', function () {
+        beforeEach(function () {
+            xrmMock.XrmMockGenerator.initialise();
+        });
+        it('Test-01', function () {
+            expect(true).toBe(true);
+        });
+    });
+});
