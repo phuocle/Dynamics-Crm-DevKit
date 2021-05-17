58f8ce878ed20c2f30a8dd39adca3cdbc5dd32ae
PhuocLe <vanphuoc@gmail.com>
2021-05-17 12:56:27 +0700

    Continue working ...

--- test/v.2.12.31/TestConsole/Dev.DevKit.Console.3/AppSettings.cs
+++ test/v.2.12.31/TestConsole/Dev.DevKit.Console.3/AppSettings.cs
@@ -2,7 +2,7 @@
 using System.Configuration;
 using Microsoft.Xrm.Tooling.Connector;
 
-namespace Dev.DevKit.Console._2
+namespace Dev.DevKit.Console._3
 {
     public static class AppSettings
     {
@@ -19,8 +19,8 @@ namespace Dev.DevKit.Console._2
         }
         private static string AuthType { get { return ConfigurationManager.AppSettings["AuthType"]; } }
         private static string Url { get { return ConfigurationManager.AppSettings["Url"]; } }
-        private static string UserName { get { return ConfigurationManager.AppSettings["Username"]; } }
-        private static string Password { get { return ConfigurationManager.AppSettings["Password"]; } }
+        private static string UserName { get { return ConfigurationManager.AppSettings["ClientId"]; } }
+        private static string Password { get { return ConfigurationManager.AppSettings["ClientSecret"]; } }
         private static string ConnectionString
         {
             get
