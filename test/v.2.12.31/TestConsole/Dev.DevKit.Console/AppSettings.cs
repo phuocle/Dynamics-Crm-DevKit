402494dd6c01b08667ca75643bad09e530461294
PhuocLe <vanphuoc@gmail.com>
2021-05-17 11:13:50 +0700

    Continue working ...

--- test/v.2.12.31/TestConsole/Dev.DevKit.Console/AppSettings.cs
+++ test/v.2.12.31/TestConsole/Dev.DevKit.Console/AppSettings.cs
@@ -1,36 +1,32 @@
 ﻿using System;
-using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Tooling.Connector;
 
 namespace Dev.DevKit.Console
 {
     public static class AppSettings
     {
-        public static IOrganizationService Service = InitializeIOrganizationService();
+        private static CrmServiceClient _Service = null;
+        public static CrmServiceClient Service {
+            get {
+                if (_Service != null) return _Service;
+                var loginForm = new LoginForm();
+                loginForm.ConnectionToCrmCompleted += loginForm_ConnectionToCrmCompleted;
+                loginForm.ShowDialog();
+                if (loginForm.CrmConnectionMgr != null && loginForm.CrmConnectionMgr.CrmSvc != null && loginForm.CrmConnectionMgr.CrmSvc.IsReady)
+                {
+                    CrmServiceClient.MaxConnectionTimeout = new TimeSpan(1, 0, 0);
+                    _Service = loginForm.CrmConnectionMgr.CrmSvc;
+                }
+                return _Service;
+            }
+        }
+
         private static void loginForm_ConnectionToCrmCompleted(object sender, EventArgs e)
         {
             if (sender is LoginForm)
             {
                 ((LoginForm)sender).Close();
             }
-        }
-
-        static IOrganizationService InitializeIOrganizationService()
-        {
-            var loginForm = new LoginForm();
-            loginForm.ConnectionToCrmCompleted += loginForm_ConnectionToCrmCompleted;
-            loginForm.ShowDialog();
-            if (loginForm.CrmConnectionMgr != null && loginForm.CrmConnectionMgr.CrmSvc != null && loginForm.CrmConnectionMgr.CrmSvc.IsReady)
-            {
-                CrmServiceClient crmServiceClient = loginForm.CrmConnectionMgr.CrmSvc;
-                if (crmServiceClient.OrganizationServiceProxy != null)
-                    return (IOrganizationService)crmServiceClient.OrganizationServiceProxy;
-                else if (crmServiceClient.OrganizationWebProxyClient != null)
-                    return (IOrganizationService)crmServiceClient.OrganizationWebProxyClient;
-                else
-                    throw new Exception("Service NULL");
-            }
-            throw new Exception("Service NULL");
         }
     }
 }
