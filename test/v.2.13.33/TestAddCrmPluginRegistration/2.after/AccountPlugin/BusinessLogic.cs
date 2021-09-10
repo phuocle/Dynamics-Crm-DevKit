c1e6009a2a0420aba73e063b6b7bb873733d2453
PhuocLe <vanphuoc@gmail.com>
2021-09-10 18:21:09 +0700

    Continue working ...

---
+++ test/v.2.13.33/TestAddCrmPluginRegistration/2.after/AccountPlugin/BusinessLogic.cs
@@ -0,0 +1,22 @@
+﻿namespace AccountPlugin
+{
+    public class BusinessLogic
+    {
+
+    }
+
+    public class ABC
+    {
+        public string A { get; set; }
+    }
+
+    public class DEF : ABC
+    {
+        public string D { get; set; }
+    }
+
+    public class GHI : DEF
+    {
+        public string G { get; set; }
+    }
+}
