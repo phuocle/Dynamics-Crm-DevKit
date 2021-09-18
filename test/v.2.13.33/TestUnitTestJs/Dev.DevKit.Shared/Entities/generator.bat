a29e6f6ca0e9b267d15cef11f4632fd5850a92d9
PhuocLe <vanphuoc@gmail.com>
2021-09-18 10:47:00 +0700

    Continue working ...

---
+++ test/v.2.13.33/TestUnitTestJs/Dev.DevKit.Shared/Entities/generator.bat
@@ -0,0 +1,8 @@
+@echo off
+for /f "delims=" %%d in ('dir /a:d /o:-n /s ..\..\packages\DynamicsCrm.DevKit.Cli.* /b') do (
+    set DynamicsCrmDevKitCli=%%d
+    goto break
+)
+:break
+set CrmConnection="AuthType=OAuth;Url=https://org0b0aa09e.crm.dynamics.com;Username=admin@CRM232358.onmicrosoft.com;Password=NZKFyxKkXVd9plGTk/0EeA==;AppId=51f81489-12ee-4a9e-aaae-a2591f45987d;RedirectUri=app://58145B91-0C36-4500-8554-080854F2AC97;LoginPrompt=Auto"
+"%DynamicsCrmDevKitCli%\tools\DynamicsCrm.DevKit.Cli.exe" /conn:%CrmConnection% /json:"..\..\DynamicsCrm.DevKit.Cli.json" /type:"generators" /profile:"LATEBOUND"
