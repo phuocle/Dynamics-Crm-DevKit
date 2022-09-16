abb88b3c3c06aa002c67b2df50fd77d2230aaecf
PhuocLe <vanphuoc@gmail.com>
2022-09-16 13:08:27 +0700

    save working

--- v2/ItemTemplates/CSharp/01.LateBoundItemTemplate/Class.cs
+++ v2/ItemTemplates/CSharp/01.LateBoundItemTemplate/Class.cs
@@ -11,11 +11,16 @@ namespace $NameSpace$
         //public DateTime? DateTimeField { get { return GetAliasedValue<DateTime?>("aliased.field"); } }
         //public EntityReference LookupField { get { return GetAliasedValue<EntityReference>("aliased.field"); } }
         //public xxxxxxxOptionSets.xxxxxxxx_code? OptionSetField { get { return (xxxxxxxOptionSets.xxxxxxxx_code?)GetAliasedValue<OptionSetValue>("aliased.field")?.Value; } }
+        //public decimal? MoneyField { get { return GetAliasedValue<Money>("aliased.field")?.Value; } }
 
         #endregion
 
         #region --- STATIC METHODS ---
 
+        public static $class$ Read_Record(IOrganizationService serviceAdmin, IOrganizationService service, ITracingService tracing, Guid? recordId)
+        {
+            throw new InvalidPluginExecutionException("Not Implemented");
+        }
 
         #endregion
     }
