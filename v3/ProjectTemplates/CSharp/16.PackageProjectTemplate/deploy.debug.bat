f9292050c65bbbb523875f7cd1c2454cfaac2a79
PhuocLe <vanphuoc@gmail.com>
2023-09-19 17:30:22 +0700

    .

--- v3/ProjectTemplates/CSharp/16.PackageProjectTemplate/deploy.debug.bat
+++ v3/ProjectTemplates/CSharp/16.PackageProjectTemplate/deploy.debug.bat
@@ -5,4 +5,4 @@ for /f "delims=" %%d in ('dir /a:d /o:-n /s ..\packages\DynamicsCrm.DevKit.Cli.*
 )
 :break
 $if$($IsOOBConnection$==0)set CrmConnection="$CrmConnectionString$"
-"%DynamicsCrmDevKitCli%\tools\DynamicsCrm.DevKit.Cli.exe" /conn:%CrmConnection% /json:"..\DynamicsCrm.DevKit.Cli.json" /type:"servers-package" /profile:"DEBUG"$endif$$if$($IsOOBConnection$==1)"%DynamicsCrmDevKitCli%\tools\DynamicsCrm.DevKit.Cli.exe" /sdklogin:"yes" /json:"..\DynamicsCrm.DevKit.Cli.json" /type:"servers-package" /profile:"DEBUG"$endif$
+"%DynamicsCrmDevKitCli%\tools\DynamicsCrm.DevKit.Cli.exe" /conn:%CrmConnection% /json:"..\DynamicsCrm.DevKit.Cli.json" /type:"servers" /profile:"DEBUG"$endif$$if$($IsOOBConnection$==1)"%DynamicsCrmDevKitCli%\tools\DynamicsCrm.DevKit.Cli.exe" /sdklogin:"yes" /json:"..\DynamicsCrm.DevKit.Cli.json" /type:"servers" /profile:"DEBUG"$endif$
