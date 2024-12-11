c8f148f55ab63e07e244c6dd7e8e9486be94d406
PhuocLe <vanphuoc@gmail.com>
2024-12-11 13:42:21 +0700

    .

--- test/3.44.44.44/UnitTestDevKitAndWebApi/Dev.DevKit.WebResource/entities/Account.webapi.js
+++ test/3.44.44.44/UnitTestDevKitAndWebApi/Dev.DevKit.WebResource/entities/Account.webapi.js
@@ -5,7 +5,7 @@ var DevKit;
 	'use strict';
 	DevKit.AccountApi = function (e) {
 		var f = '@OData.Community.Display.V1.FormattedValue';
-		function webApiField(obj, field, entity, logicalName, schemaName, entityLogicalCollectionName, entityLogicalName, readOnly, upsertEntity, isMultiOptionSet, type) {
+		function webApiField(obj, field, entity, logicalName, schemaName, entityLogicalCollectionName, entityLogicalName, readOnly, upsertEntity, type) {
 			var l = '@Microsoft.Dynamics.CRM.lookuplogicalname';
 			var getFormattedValue = function () {
 				if (entity[logicalName + f] === undefined || entity[logicalName + f] === null) {
@@ -17,7 +17,7 @@ var DevKit;
 					}
 					return '';
 				}
-				if (isMultiOptionSet) {
+				if (type === 'MultiOptionSet') {
 					return entity[logicalName + f].toString().split(';').map(function (item) { return item.trim(); });
 				}
 				return entity[logicalName + f];
@@ -32,17 +32,57 @@ var DevKit;
 					}
 					return null;
 				}
-				if (isMultiOptionSet) {
+				if (type === 'MultiOptionSet') {
 					return entity[logicalName].toString().split(',').map(function (item) { return parseInt(item, 10); });
 				}
 				return returnGet(entity[logicalName], type);
 			};
 			var returnGet = function (data, type) {
-				if (type === "date") return parseDate(data);
+				var parseDate = function (dateString) {
+					const date = new Date(dateString);
+					if (isNaN(date.getTime())) {
+						return null;
+					} else {
+						return date;
+					}
+				}
+				var parseBoolean = function (booleanString) {
+					if (typeof booleanString !== 'string') return false;
+					const lowerStr = booleanString.trim().toLowerCase();
+					const trueValues = ["true", "1", "yes", "y"];
+					const falseValues = ["false", "0", "no", "n"];
+					if (trueValues.includes(lowerStr)) {
+						return true;
+					} else if (falseValues.includes(lowerStr)) {
+						return false;
+					} else {
+						return false;
+					}
+				};
+				if (data === null || data === undefined) return null;
+				if (type === null || type === undefined) return data;
+				if (type === "DateTime") {
+					const date = parseDate(data);
+					if (isNaN(date.getTime())) return null;
+					return date;
+				}
+				else if (type === "Integer") {
+					const int = parseInt(data);
+					if (isNaN(int)) return null;
+					return int;
+				}
+				else if (type === "Number") {
+					const double = Number(data);
+					if (isNaN(double)) return null;
+					return double;
+				}
+				else if (type === "Boolean") {
+					return parseBoolean(data);
+				}
 				return data;
 			}
 			var setValue = function (value) {
-				if (isMultiOptionSet) value = value.join(',');
+				if (type === 'MultiOptionSet') value = value.join(',');
 				if (entityLogicalCollectionName !== undefined && entityLogicalCollectionName.length > 0) {
 					if (value === null) {
 						upsertEntity[schemaName + '@odata.bind'] = null;
@@ -56,14 +96,6 @@ var DevKit;
 				}
 				entity[logicalName] = value;
 			};
-			var parseDate = function (dateString) {
-				const date = new Date(dateString);
-				if (isNaN(date.getTime())) {
-					return null;
-				} else {
-					return date;
-				}
-			}
 			Object.defineProperty(obj.FormattedValue, field, {
 				get: getFormattedValue
 			});
@@ -80,88 +112,88 @@ var DevKit;
 			}
 		}
 		var _account = {
-			AccountCategoryCode: { a: 'accountcategorycode' },
-			AccountClassificationCode: { a: 'accountclassificationcode' },
+			AccountCategoryCode: { a: 'accountcategorycode', g: 'Integer' },
+			AccountClassificationCode: { a: 'accountclassificationcode', g: 'Integer' },
 			AccountId: { a: 'accountid' },
 			AccountNumber: { a: 'accountnumber' },
-			AccountRatingCode: { a: 'accountratingcode' },
+			AccountRatingCode: { a: 'accountratingcode', g: 'Integer' },
 			Address1_AddressId: { a: 'address1_addressid' },
-			Address1_AddressTypeCode: { a: 'address1_addresstypecode' },
+			Address1_AddressTypeCode: { a: 'address1_addresstypecode', g: 'Integer' },
 			Address1_City: { a: 'address1_city' },
 			Address1_Composite: { a: 'address1_composite', r: true },
 			Address1_Country: { a: 'address1_country' },
 			Address1_County: { a: 'address1_county' },
 			Address1_Fax: { a: 'address1_fax' },
-			Address1_FreightTermsCode: { a: 'address1_freighttermscode' },
-			Address1_Latitude: { a: 'address1_latitude' },
+			Address1_FreightTermsCode: { a: 'address1_freighttermscode', g: 'Integer' },
+			Address1_Latitude: { a: 'address1_latitude', g: 'Number' },
 			Address1_Line1: { a: 'address1_line1' },
 			Address1_Line2: { a: 'address1_line2' },
 			Address1_Line3: { a: 'address1_line3' },
-			Address1_Longitude: { a: 'address1_longitude' },
+			Address1_Longitude: { a: 'address1_longitude', g: 'Number' },
 			Address1_Name: { a: 'address1_name' },
 			Address1_PostalCode: { a: 'address1_postalcode' },
 			Address1_PostOfficeBox: { a: 'address1_postofficebox' },
 			Address1_PrimaryContactName: { a: 'address1_primarycontactname' },
-			Address1_ShippingMethodCode: { a: 'address1_shippingmethodcode' },
+			Address1_ShippingMethodCode: { a: 'address1_shippingmethodcode', g: 'Integer' },
 			Address1_StateOrProvince: { a: 'address1_stateorprovince' },
 			Address1_Telephone1: { a: 'address1_telephone1' },
 			Address1_Telephone2: { a: 'address1_telephone2' },
 			Address1_Telephone3: { a: 'address1_telephone3' },
 			Address1_UPSZone: { a: 'address1_upszone' },
-			Address1_UTCOffset: { a: 'address1_utcoffset' },
+			Address1_UTCOffset: { a: 'address1_utcoffset', g: 'Integer' },
 			Address2_AddressId: { a: 'address2_addressid' },
-			Address2_AddressTypeCode: { a: 'address2_addresstypecode' },
+			Address2_AddressTypeCode: { a: 'address2_addresstypecode', g: 'Integer' },
 			Address2_City: { a: 'address2_city' },
 			Address2_Composite: { a: 'address2_composite', r: true },
 			Address2_Country: { a: 'address2_country' },
 			Address2_County: { a: 'address2_county' },
 			Address2_Fax: { a: 'address2_fax' },
-			Address2_FreightTermsCode: { a: 'address2_freighttermscode' },
-			Address2_Latitude: { a: 'address2_latitude' },
+			Address2_FreightTermsCode: { a: 'address2_freighttermscode', g: 'Integer' },
+			Address2_Latitude: { a: 'address2_latitude', g: 'Number' },
 			Address2_Line1: { a: 'address2_line1' },
 			Address2_Line2: { a: 'address2_line2' },
 			Address2_Line3: { a: 'address2_line3' },
-			Address2_Longitude: { a: 'address2_longitude' },
+			Address2_Longitude: { a: 'address2_longitude', g: 'Number' },
 			Address2_Name: { a: 'address2_name' },
 			Address2_PostalCode: { a: 'address2_postalcode' },
 			Address2_PostOfficeBox: { a: 'address2_postofficebox' },
 			Address2_PrimaryContactName: { a: 'address2_primarycontactname' },
-			Address2_ShippingMethodCode: { a: 'address2_shippingmethodcode' },
+			Address2_ShippingMethodCode: { a: 'address2_shippingmethodcode', g: 'Integer' },
 			Address2_StateOrProvince: { a: 'address2_stateorprovince' },
 			Address2_Telephone1: { a: 'address2_telephone1' },
 			Address2_Telephone2: { a: 'address2_telephone2' },
 			Address2_Telephone3: { a: 'address2_telephone3' },
 			Address2_UPSZone: { a: 'address2_upszone' },
-			Address2_UTCOffset: { a: 'address2_utcoffset' },
+			Address2_UTCOffset: { a: 'address2_utcoffset', g: 'Integer' },
 			Adx_CreatedByIPAddress: { a: 'adx_createdbyipaddress' },
 			Adx_CreatedByUsername: { a: 'adx_createdbyusername' },
 			Adx_ModifiedByIPAddress: { a: 'adx_modifiedbyipaddress' },
 			Adx_ModifiedByUsername: { a: 'adx_modifiedbyusername' },
-			Aging30: { a: 'aging30', r: true },
-			Aging30_Base: { a: 'aging30_base', r: true },
-			Aging60: { a: 'aging60', r: true },
-			Aging60_Base: { a: 'aging60_base', r: true },
-			Aging90: { a: 'aging90', r: true },
-			Aging90_Base: { a: 'aging90_base', r: true },
-			BusinessTypeCode: { a: 'businesstypecode' },
+			Aging30: { a: 'aging30', r: true, g: 'Number' },
+			Aging30_Base: { a: 'aging30_base', r: true, g: 'Number' },
+			Aging60: { a: 'aging60', r: true, g: 'Number' },
+			Aging60_Base: { a: 'aging60_base', r: true, g: 'Number' },
+			Aging90: { a: 'aging90', r: true, g: 'Number' },
+			Aging90_Base: { a: 'aging90_base', r: true, g: 'Number' },
+			BusinessTypeCode: { a: 'businesstypecode', g: 'Integer' },
 			CreatedBy: { b: 'createdby', a: '_createdby_value', c: 'systemusers', d: 'systemuser', r: true },
 			CreatedByExternalParty: { b: 'createdbyexternalparty', a: '_createdbyexternalparty_value', c: 'externalparties', d: 'externalparty', r: true },
-			CreatedOn_UtcDateAndTime: { a: 'createdon', r: true },
+			CreatedOn_UtcDateAndTime: { a: 'createdon', r: true, g: 'DateTime' },
 			CreatedOnBehalfBy: { b: 'createdonbehalfby', a: '_createdonbehalfby_value', c: 'systemusers', d: 'systemuser', r: true },
-			CreditLimit: { a: 'creditlimit' },
-			CreditLimit_Base: { a: 'creditlimit_base', r: true },
-			CreditOnHold: { a: 'creditonhold' },
-			CustomerSizeCode: { a: 'customersizecode' },
-			CustomerTypeCode: { a: 'customertypecode' },
+			CreditLimit: { a: 'creditlimit', g: 'Number' },
+			CreditLimit_Base: { a: 'creditlimit_base', r: true, g: 'Number' },
+			CreditOnHold: { a: 'creditonhold', g: 'Boolean' },
+			CustomerSizeCode: { a: 'customersizecode', g: 'Integer' },
+			CustomerTypeCode: { a: 'customertypecode', g: 'Integer' },
 			Description: { a: 'description' },
-			devkit_BigInt: { a: 'devkit_bigint' },
-			DoNotBulkEMail: { a: 'donotbulkemail' },
-			DoNotBulkPostalMail: { a: 'donotbulkpostalmail' },
-			DoNotEMail: { a: 'donotemail' },
-			DoNotFax: { a: 'donotfax' },
-			DoNotPhone: { a: 'donotphone' },
-			DoNotPostalMail: { a: 'donotpostalmail' },
-			DoNotSendMM: { a: 'donotsendmm' },
+			devkit_BigInt: { a: 'devkit_bigint', g: 'Integer' },
+			DoNotBulkEMail: { a: 'donotbulkemail', g: 'Boolean' },
+			DoNotBulkPostalMail: { a: 'donotbulkpostalmail', g: 'Boolean' },
+			DoNotEMail: { a: 'donotemail', g: 'Boolean' },
+			DoNotFax: { a: 'donotfax', g: 'Boolean' },
+			DoNotPhone: { a: 'donotphone', g: 'Boolean' },
+			DoNotPostalMail: { a: 'donotpostalmail', g: 'Boolean' },
+			DoNotSendMM: { a: 'donotsendmm', g: 'Boolean' },
 			EMailAddress1: { a: 'emailaddress1' },
 			EMailAddress2: { a: 'emailaddress2' },
 			EMailAddress3: { a: 'emailaddress3' },
@@ -169,68 +201,68 @@ var DevKit;
 			EntityImage_Timestamp: { a: 'entityimage_timestamp', r: true },
 			EntityImage_URL: { a: 'entityimage_url', r: true },
 			EntityImageId: { a: 'entityimageid', r: true },
-			ExchangeRate: { a: 'exchangerate', r: true },
+			ExchangeRate: { a: 'exchangerate', r: true, g: 'Number' },
 			Fax: { a: 'fax' },
-			FollowEmail: { a: 'followemail' },
+			FollowEmail: { a: 'followemail', g: 'Boolean' },
 			FtpSiteURL: { a: 'ftpsiteurl' },
-			ImportSequenceNumber: { a: 'importsequencenumber' },
-			IndustryCode: { a: 'industrycode' },
-			IsPrivate: { a: 'isprivate', r: true },
-			LastOnHoldTime_UtcDateAndTime: { a: 'lastonholdtime' },
-			LastUsedInCampaign_UtcDateOnly: { a: 'lastusedincampaign' },
-			MarketCap: { a: 'marketcap' },
-			MarketCap_Base: { a: 'marketcap_base', r: true },
-			MarketingOnly: { a: 'marketingonly' },
+			ImportSequenceNumber: { a: 'importsequencenumber', g: 'Integer' },
+			IndustryCode: { a: 'industrycode', g: 'Integer' },
+			IsPrivate: { a: 'isprivate', r: true, g: 'Boolean' },
+			LastOnHoldTime_UtcDateAndTime: { a: 'lastonholdtime', g: 'DateTime' },
+			LastUsedInCampaign_UtcDateOnly: { a: 'lastusedincampaign', g: 'DateTime' },
+			MarketCap: { a: 'marketcap', g: 'Number' },
+			MarketCap_Base: { a: 'marketcap_base', r: true, g: 'Number' },
+			MarketingOnly: { a: 'marketingonly', g: 'Boolean' },
 			MasterId: { b: 'masterid', a: '_masterid_value', c: 'accounts', d: 'account', r: true },
-			Merged: { a: 'merged', r: true },
+			Merged: { a: 'merged', r: true, g: 'Boolean' },
 			ModifiedBy: { b: 'modifiedby', a: '_modifiedby_value', c: 'systemusers', d: 'systemuser', r: true },
 			ModifiedByExternalParty: { b: 'modifiedbyexternalparty', a: '_modifiedbyexternalparty_value', c: 'externalparties', d: 'externalparty', r: true },
-			ModifiedOn_UtcDateAndTime: { a: 'modifiedon', r: true, t: 'date' },
+			ModifiedOn_UtcDateAndTime: { a: 'modifiedon', r: true, g: 'DateTime' },
 			ModifiedOnBehalfBy: { b: 'modifiedonbehalfby', a: '_modifiedonbehalfby_value', c: 'systemusers', d: 'systemuser', r: true },
 			msa_managingpartnerid: { b: 'msa_managingpartnerid', a: '_msa_managingpartnerid_value', c: 'accounts', d: 'account' },
 			Name: { a: 'name' },
-			NumberOfEmployees: { a: 'numberofemployees' },
-			OnHoldTime: { a: 'onholdtime', r: true },
-			OverriddenCreatedOn_UtcDateOnly: { a: 'overriddencreatedon' },
+			NumberOfEmployees: { a: 'numberofemployees', g: 'Integer' },
+			OnHoldTime: { a: 'onholdtime', r: true, g: 'Integer' },
+			OverriddenCreatedOn_UtcDateOnly: { a: 'overriddencreatedon', g: 'DateTime' },
 			OwnerId_systemuser: { b: 'ownerid', a: '_ownerid_value', c: 'systemusers', d: 'systemuser' },
 			OwnerId_team: { b: 'ownerid', a: '_ownerid_value', c: 'teams', d: 'team' },
-			OwnershipCode: { a: 'ownershipcode' },
+			OwnershipCode: { a: 'ownershipcode', g: 'Integer' },
 			OwningBusinessUnit: { b: 'owningbusinessunit', a: '_owningbusinessunit_value', c: 'businessunits', d: 'businessunit', r: true },
 			OwningTeam: { b: 'owningteam', a: '_owningteam_value', c: 'teams', d: 'team', r: true },
 			OwningUser: { b: 'owninguser', a: '_owninguser_value', c: 'systemusers', d: 'systemuser', r: true },
 			ParentAccountId: { b: 'parentaccountid', a: '_parentaccountid_value', c: 'accounts', d: 'account' },
-			ParticipatesInWorkflow: { a: 'participatesinworkflow' },
-			PaymentTermsCode: { a: 'paymenttermscode' },
-			PreferredAppointmentDayCode: { a: 'preferredappointmentdaycode' },
-			PreferredAppointmentTimeCode: { a: 'preferredappointmenttimecode' },
-			PreferredContactMethodCode: { a: 'preferredcontactmethodcode' },
+			ParticipatesInWorkflow: { a: 'participatesinworkflow', g: 'Boolean' },
+			PaymentTermsCode: { a: 'paymenttermscode', g: 'Integer' },
+			PreferredAppointmentDayCode: { a: 'preferredappointmentdaycode', g: 'Integer' },
+			PreferredAppointmentTimeCode: { a: 'preferredappointmenttimecode', g: 'Integer' },
+			PreferredContactMethodCode: { a: 'preferredcontactmethodcode', g: 'Integer' },
 			PreferredSystemUserId: { b: 'preferredsystemuserid', a: '_preferredsystemuserid_value', c: 'systemusers', d: 'systemuser' },
 			PrimaryContactId: { b: 'primarycontactid', a: '_primarycontactid_value', c: 'contacts', d: 'contact' },
 			PrimarySatoriId: { a: 'primarysatoriid' },
 			PrimaryTwitterId: { a: 'primarytwitterid' },
 			ProcessId: { a: 'processid' },
-			Revenue: { a: 'revenue' },
-			Revenue_Base: { a: 'revenue_base', r: true },
-			SharesOutstanding: { a: 'sharesoutstanding' },
-			ShippingMethodCode: { a: 'shippingmethodcode' },
+			Revenue: { a: 'revenue', g: 'Number' },
+			Revenue_Base: { a: 'revenue_base', r: true, g: 'Number' },
+			SharesOutstanding: { a: 'sharesoutstanding', g: 'Integer' },
+			ShippingMethodCode: { a: 'shippingmethodcode', g: 'Integer' },
 			SIC: { a: 'sic' },
 			SLAId: { b: 'slaid', a: '_slaid_value', c: 'slas', d: 'sla' },
 			SLAInvokedId: { b: 'slainvokedid', a: '_slainvokedid_value', c: 'slas', d: 'sla', r: true },
 			StageId: { a: 'stageid' },
-			StateCode: { a: 'statecode' },
-			StatusCode: { a: 'statuscode' },
+			StateCode: { a: 'statecode', g: 'Integer' },
+			StatusCode: { a: 'statuscode', g: 'Integer' },
 			StockExchange: { a: 'stockexchange' },
 			Telephone1: { a: 'telephone1' },
 			Telephone2: { a: 'telephone2' },
 			Telephone3: { a: 'telephone3' },
-			TerritoryCode: { a: 'territorycode' },
+			TerritoryCode: { a: 'territorycode', g: 'Integer' },
 			TickerSymbol: { a: 'tickersymbol' },
 			TimeSpentByMeOnEmailAndMeetings: { a: 'timespentbymeonemailandmeetings', r: true },
-			TimeZoneRuleVersionNumber: { a: 'timezoneruleversionnumber' },
+			TimeZoneRuleVersionNumber: { a: 'timezoneruleversionnumber', g: 'Integer' },
 			TransactionCurrencyId: { b: 'transactioncurrencyid', a: '_transactioncurrencyid_value', c: 'transactioncurrencies', d: 'transactioncurrency' },
 			TraversedPath: { a: 'traversedpath' },
-			UTCConversionTimeZoneCode: { a: 'utcconversiontimezonecode' },
-			VersionNumber: { a: 'versionnumber', r: true },
+			UTCConversionTimeZoneCode: { a: 'utcconversiontimezonecode', g: 'Integer' },
+			VersionNumber: { a: 'versionnumber', r: true, g: 'Integer' },
 			WebSiteURL: { a: 'websiteurl' },
 			YomiName: { a: 'yominame' }
 		};
@@ -246,8 +278,7 @@ var DevKit;
 			var d = _account[field].d;
 			var g = _account[field].g;
 			var r = _account[field].r;
-			var t = _account[field].t;
-			webApiField(account, field, e, a, b, c, d, r, u, g, t);
+			webApiField(account, field, e, a, b, c, d, r, u, g);
 		}
 		account.Entity = u;
 		account.EntityName = 'account';
