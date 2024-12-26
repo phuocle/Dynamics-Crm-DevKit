1bd8fc7fe54d9660f4c245e5d02a402ff747c0ef
PhuocLe <vanphuoc@gmail.com>
2024-12-26 13:21:34 +0700

    .

---
+++ test/3.44.44.44/TestAllProjects2/Dev.DevKit.Shared.Test/FakeXrmEasyTestBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Dev.DevKit.ProxyTypes;
+using FakeXrmEasy.Abstractions;
+using FakeXrmEasy.Abstractions.Enums;
+using FakeXrmEasy.Middleware;
+using Microsoft.Xrm.Sdk;
+
+namespace Dev.DevKit.Shared.Test
+{
+    public class FakeXrmEasyTestBase
+    {
+        protected readonly IXrmFakedContext _context;
+        protected readonly IOrganizationService _service;
+
+        public FakeXrmEasyTestBase()
+        {
+            _context = XrmFakedContextFactory.New(FakeXrmEasyLicense.RPL_1_5);
+            _context.EnableProxyTypes(Assembly.GetAssembly(typeof(ProxyTypesAssembly)));
+            _service = _context.GetOrganizationService();
+        }
+    }
+}
