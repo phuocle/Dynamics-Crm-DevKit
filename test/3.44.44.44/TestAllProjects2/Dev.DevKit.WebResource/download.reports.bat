1c6f54a1c4eba811a55546c9a4f0219ff7af34be
PhuocLe <vanphuoc@gmail.com>
2024-12-26 13:49:01 +0700

    .

---
+++ test/3.44.44.44/TestAllProjects2/Dev.DevKit.WebResource/download.reports.bat
@@ -0,0 +1,8 @@
+@echo off
+for /f "delims=" %%d in ('dir /a:d /o:-n /s ..\packages\DynamicsCrm.DevKit.Cli.* /b') do (
+    set DynamicsCrmDevKitCli=%%d
+    goto break
+)
+:break
+set CrmConnection="AuthType=ClientSecret;Url=https://dynamics-crm-devkit.crm5.dynamics.com;ClientId=1a60a5c2-d04c-4b26-8f86-9d6ce0616799;ClientSecret=~je8Q~4DL221zUgKOaHq-EWMlowkpl3KEbZItccL;"
+"%DynamicsCrmDevKitCli%\tools\DynamicsCrm.DevKit.Cli.exe" /conn:%CrmConnection% /json:"..\DynamicsCrm.DevKit.Cli.json" /type:"downloadreports" /profile:"DEBUG"
