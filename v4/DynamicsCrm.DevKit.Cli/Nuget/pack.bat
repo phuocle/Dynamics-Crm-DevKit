a18a3d49d2ec4e8ea6eef2dd324a648e5c7024ba
PhuocLe <vanphuoc@gmail.com>
2025-08-06 08:31:58 +0700

    .

--- v4/DynamicsCrm.DevKit.Cli/Nuget/pack.bat
+++ v4/DynamicsCrm.DevKit.Cli/Nuget/pack.bat
@@ -1,5 +1,6 @@
 @echo off
 set /p VERSION=<..\..\version.txt
 nuget pack DynamicsCrm.DevKit.Cli.nuspec -Tool -Version %VERSION%
+ren DynamicsCrm.DevKit.Cli.4.0.0.nupkg DynamicsCrm.DevKit.Cli.%VERSION%.nupkg
 if not exist ..\..\Published\%VERSION% mkdir ..\..\Published\%VERSION%
 copy DynamicsCrm.DevKit.Cli.%VERSION%.nupkg ..\..\Published\%VERSION%\DynamicsCrm.DevKit.Cli.%VERSION%.nupkg /y
