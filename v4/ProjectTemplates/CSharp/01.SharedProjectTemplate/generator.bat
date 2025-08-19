9d45f613fecfe010b4c99a039d38c5c653e2471f
PhuocLe <vanphuoc@gmail.com>
2025-08-19 14:08:07 +0700

    .

--- v4/ProjectTemplates/CSharp/01.SharedProjectTemplate/generator.bat
+++ v4/ProjectTemplates/CSharp/01.SharedProjectTemplate/generator.bat
@@ -1,8 +1,4 @@
 @echo off
-for /f "delims=" %%d in ('dir /a:d /o:-n /s ..\..\packages\DynamicsCrm.DevKit.Cli.* /b') do (
-    set DynamicsCrmDevKitCli=%%d
-    goto break
-)
-:break
+for /f "delims=" %%d in ('dir /a:d /o:-n /s ..\..\packages\DynamicsCrm.DevKit.Cli.* /b 2^>nul') do if not defined DynamicsCrmDevKitCli set DynamicsCrmDevKitCli=%%d
 set CrmConnection="$ConnectionString$"
 "%DynamicsCrmDevKitCli%\tools\DynamicsCrm.DevKit.Cli.exe" /conn:%CrmConnection% /json:"..\..\DynamicsCrm.DevKit.Cli.json" /type:"generators" /profile:"LATEBOUND"
