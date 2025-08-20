5e78080883b0501b75415ef595e5399e2d241202
PhuocLe <vanphuoc@gmail.com>
2025-08-20 09:05:35 +0700

    .

---
+++ test/4.00.00.00/TestAllProjects/Dev.DevKit.Shared/Entities/generator.bat
@@ -0,0 +1,4 @@
+@echo off
+for /f "delims=" %%d in ('dir /a:d /o:-n /s ..\..\packages\DynamicsCrm.DevKit.Cli.* /b 2^>nul') do if not defined DynamicsCrmDevKitCli set DynamicsCrmDevKitCli=%%d
+set CrmConnection="AuthType=ClientSecret;Url=https://dynamics-crm-devkit.crm5.dynamics.com;ClientId=1a60a5c2-d04c-4b26-8f86-9d6ce0616799;ClientSecret=4Y11hDyKJYQTqXC9cRDXnoJ2DytZDs/jYI1byYwKli57mRfjHcCPu6Qx5sxgtCWQ;"
+"%DynamicsCrmDevKitCli%\tools\DynamicsCrm.DevKit.Cli.exe" /conn:%CrmConnection% /json:"..\..\DynamicsCrm.DevKit.Cli.json" /type:"generators" /profile:"LATEBOUND"
