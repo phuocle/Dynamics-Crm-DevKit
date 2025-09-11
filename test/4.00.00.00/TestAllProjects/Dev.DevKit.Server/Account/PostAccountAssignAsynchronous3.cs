34884a192b4a96eb362bd64523f0573f2c81b79e
PhuocLe <vanphuoc@gmail.com>
2025-09-11 14:11:27 +0700

    .

---
+++ test/4.00.00.00/TestAllProjects/Dev.DevKit.Server/Account/PostAccountAssignAsynchronous3.cs
@@ -0,0 +1,50 @@
+﻿using Dev.DevKit.Shared;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Extensions;
+using System;
+
+namespace Dev.DevKit.Server.Account
+{
+    [CrmPluginRegistration("Assign", "account", StageEnum.PostOperation, ExecutionModeEnum.Asynchronous, "", "Dev.DevKit.Server.Account.PostAccountAssignAsynchronous3", 3, IsolationModeEnum.Sandbox, PluginType = PluginType.Plugin, DeleteAsyncOperation = true)]
+    public class PostAccountAssignAsynchronous3 : IPlugin
+    {
+        /* This is custom template for Plugin 1
+        InputParameters:
+            Target      Microsoft.Xrm.Sdk.EntityReference - require
+            Assignee    Microsoft.Xrm.Sdk.EntityReference - require
+        OutputParameters:
+        */
+
+        //private readonly string unSecureConfiguration = null;
+        //private readonly string secureConfiguration = null;
+        //public PostAccountAssignAsynchronous3(string unSecureConfiguration, string secureConfiguration)
+        //{
+        //    this.unSecureConfiguration = unSecureConfiguration;
+        //    this.secureConfiguration = secureConfiguration;
+        //}
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var tracing = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            if (!int.Equals(context.Stage, (int)StageEnum.PostOperation)) throw new InvalidPluginExecutionException("Stage does not equals PostOperation");
+            if (!string.Equals(context.MessageName, "Assign", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("MessageName does not equals Assign");
+            if (!string.Equals(context.PrimaryEntityName, "account", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("PrimaryEntityName does not equals account");
+            if (!int.Equals(context.Mode, (int)ExecutionModeEnum.Asynchronous)) throw new InvalidPluginExecutionException("Execution does not equals Asynchronous");
+            var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var serviceAdmin = serviceFactory.CreateOrganizationService(null);
+            var service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            tracing?.DebugContext(context);
+
+            ExecutePlugin(context, serviceFactory, serviceAdmin, service, tracing);
+        }
+
+        private void ExecutePlugin(IPluginExecutionContext context, IOrganizationServiceFactory serviceFactory, IOrganizationService serviceAdmin, IOrganizationService service, ITracingService tracing)
+        {
+            //var ??? = context.InputParameterOrDefault<???>("???");
+            //YOUR PLUGIN-CODE GO HERE
+
+        }
+    }
+}
--- test/4.00.00.00/TestAllProjects/Dev.DevKit.Server/Account/PostAccountAssignAsynchronous4.cs
+++ test/4.00.00.00/TestAllProjects/Dev.DevKit.Server/Account/PostAccountAssignAsynchronous4.cs
@@ -5,10 +5,10 @@ using System;
 
 namespace Dev.DevKit.Server.Account
 {
-    [CrmPluginRegistration("Assign", "account", StageEnum.PostOperation, ExecutionModeEnum.Asynchronous, "", "Dev.DevKit.Server.Account.PostAccountAssignAsynchronous", 2, IsolationModeEnum.Sandbox, PluginType = PluginType.Plugin, DeleteAsyncOperation = true)]
-    public class PostAccountAssignAsynchronous2 : IPlugin
+    [CrmPluginRegistration("Assign", "account", StageEnum.PostOperation, ExecutionModeEnum.Asynchronous, "", "Dev.DevKit.Server.Account.PostAccountAssignAsynchronous4", 4, IsolationModeEnum.Sandbox, PluginType = PluginType.Plugin, DeleteAsyncOperation = true)]
+    public class PostAccountAssignAsynchronous4 : IPlugin
     {
-        /* PL1
+        /* This is custom template for Plugin 2
         InputParameters:
             Target      Microsoft.Xrm.Sdk.EntityReference - require
             Assignee    Microsoft.Xrm.Sdk.EntityReference - require
@@ -17,7 +17,7 @@ namespace Dev.DevKit.Server.Account
 
         //private readonly string unSecureConfiguration = null;
         //private readonly string secureConfiguration = null;
-        //public PostAccountAssignAsynchronous(string unSecureConfiguration, string secureConfiguration)
+        //public PostAccountAssignAsynchronous4(string unSecureConfiguration, string secureConfiguration)
         //{
         //    this.unSecureConfiguration = unSecureConfiguration;
         //    this.secureConfiguration = secureConfiguration;
@@ -26,16 +26,16 @@ namespace Dev.DevKit.Server.Account
         public void Execute(IServiceProvider serviceProvider)
         {
             var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var tracing = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            if (!int.Equals(context.Stage, (int)StageEnum.PostOperation)) throw new InvalidPluginExecutionException("Stage does not equals PostOperation");
+            if (!string.Equals(context.MessageName, "Assign", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("MessageName does not equals Assign");
+            if (!string.Equals(context.PrimaryEntityName, "account", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("PrimaryEntityName does not equals account");
+            if (!int.Equals(context.Mode, (int)ExecutionModeEnum.Asynchronous)) throw new InvalidPluginExecutionException("Execution does not equals Asynchronous");
             var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
             var serviceAdmin = serviceFactory.CreateOrganizationService(null);
             var service = serviceFactory.CreateOrganizationService(context.UserId);
-            var tracing = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
-            if (context.Stage != (int)StageEnum.PostOperation) throw new InvalidPluginExecutionException("Stage does not equals PostOperation");
-            if (context.PrimaryEntityName.ToLower() != "account") throw new InvalidPluginExecutionException("PrimaryEntityName does not equals account");
-            if (context.MessageName.ToLower() != "Assign".ToLower()) throw new InvalidPluginExecutionException("MessageName does not equals Assign");
-            if (context.Mode != (int)ExecutionModeEnum.Asynchronous) throw new InvalidPluginExecutionException("Execution does not equals Asynchronous");
 
-            tracing.DebugContext(context);
+            tracing?.DebugContext(context);
 
             ExecutePlugin(context, serviceFactory, serviceAdmin, service, tracing);
         }
@@ -44,6 +44,7 @@ namespace Dev.DevKit.Server.Account
         {
             //var ??? = context.InputParameterOrDefault<???>("???");
             //YOUR PLUGIN-CODE GO HERE
+
         }
     }
 }
