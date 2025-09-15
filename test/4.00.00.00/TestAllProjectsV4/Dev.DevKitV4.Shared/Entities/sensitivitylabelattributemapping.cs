5f498b90b63b9386aca8153e667c22d153ef538c
PhuocLe <vanphuoc@gmail.com>
2025-09-16 06:27:28 +0700

    .

---
+++ test/4.00.00.00/TestAllProjectsV4/Dev.DevKitV4.Shared/Entities/sensitivitylabelattributemapping.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace Dev.DevKitV4.Shared.Entities
+{
+	public partial class sensitivitylabelattributemapping
+	{
+		#region --- PROPERTIES ---
+
+		//public string StringField { get { return GetAliasedValue<string>("aliased.field"); } }
+		//public int? IntField { get { return GetAliasedValue<int?>("aliased.field"); } }
+		//public DateTime? DateTimeField { get { return GetAliasedValue<DateTime?>("aliased.field"); } }
+		//public EntityReference LookupField { get { return GetAliasedValue<EntityReference>("aliased.field"); } }
+		//public xxxOptionSets.xxx? OptionSetField { get { return (xxxOptionSets.xxx?)GetAliasedValue<OptionSetValue>("aliased.field")?.Value; } }
+		//public decimal? MoneyField { get { return GetAliasedValue<Money>("aliased.field")?.Value; } }
+
+		#endregion
+
+		#region --- STATIC METHODS ---
+
+		public static sensitivitylabelattributemapping Read_Record(IOrganizationService serviceAdmin, IOrganizationService service, ITracingService tracing, Guid? recordId)
+		{
+			var fetchData = new
+			{
+				sensitivitylabelattributemappingid = recordId ?? Guid.Empty
+			};
+			var fetchXml = $@"
+<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
+  <entity name='sensitivitylabelattributemapping'>
+    <all-attributes/>
+    <filter type='and'>
+      <condition attribute='sensitivitylabelattributemappingid' operator='eq' value='{fetchData.sensitivitylabelattributemappingid}'/>
+    </filter>
+  </entity>
+</fetch>
+";
+			var rows = serviceAdmin.RetrieveMultiple<sensitivitylabelattributemapping>(fetchXml);
+			if (rows.Count == 1) return rows[0];
+			return new sensitivitylabelattributemapping();
+		}
+
+		#endregion
+	}
+}
