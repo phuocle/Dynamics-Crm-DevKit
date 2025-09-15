8359f0cbaae9ac21c344185c279453f3c801dbf0
PhuocLe <vanphuoc@gmail.com>
2025-09-15 15:27:35 +0700

    .

---
+++ test/4.00.00.00/TestAllProjectsV4/Dev.DevKitV4.OOB.Console/Lib/TracingServiceFake.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xrm.Sdk;
+
+namespace Dev.DevKitV4.OOB.Console.Lib
+{
+    public class TracingServiceFake : ITracingService
+    {
+        public ITracingService tracingService;
+
+        public void Trace(string format, params object[] args)
+        {
+            if (tracingService == null)
+            {
+                try
+                {
+                    System.Console.Write(format, args);
+                    System.Console.WriteLine();
+                }
+                catch
+                {
+                    System.Console.Write(format);
+                    System.Console.WriteLine();
+                }
+            }
+            else
+            {
+                tracingService.Trace(format, args);
+            }
+        }
+    }
+}
