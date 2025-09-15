78345826e84ba2a268653e60179695ce31e53327
PhuocLe <vanphuoc@gmail.com>
2025-09-15 15:45:03 +0700

    .

---
+++ test/4.00.00.00/TestAllProjectsV4/Dev.DevKitV4.Edit.ConsoleCore.ClientSecret/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using System;
+
+namespace Dev.DevKitV4.Edit.ConsoleCore.ClientSecret
+{
+    public class Program
+    {
+        static void Main()
+        {
+            CheckWhoAmI();
+            //DebugPlugin();
+        }
+
+        private static void DebugPlugin()
+        {
+            //var json = "";
+            //var serviceProvider = Helper.GetServiceProvider(json, App.Service);
+            //var plugin = new Abc.Xyz.PluginXXX.PostCreateAccountSynchronous();
+            //plugin.Execute(serviceProvider);
+        }
+
+        private static void CheckWhoAmI()
+        {
+            var UserId = ((WhoAmIResponse)App.Service.Execute(new WhoAmIRequest())).UserId;
+            System.Console.WriteLine(UserId);
+            System.Console.ReadKey();
+        }
+    }
+}
