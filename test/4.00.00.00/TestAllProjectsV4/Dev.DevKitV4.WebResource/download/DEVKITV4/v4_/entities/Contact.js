12d5d58bc99734a936079a66f19512483334557d
PhuocLe <vanphuoc@gmail.com>
2025-09-19 14:48:47 +0700

    .

---
+++ test/4.00.00.00/TestAllProjectsV4/Dev.DevKitV4.WebResource/download/DEVKITV4/v4_/entities/Contact.js
@@ -0,0 +1,220 @@
+﻿//@ts-check
+///<reference path="Contact.d.ts" />
+"use strict";
+var formContact = (function () {
+	"use strict";
+	/** @type DevKitV4.FormContact */
+	var form = null;
+	/** @param {any} executionContext */
+	async function onLoad(executionContext) {
+		form = new DevKitV4.FormContact(executionContext);
+		registerEvents();
+		form.UiAddLoaded(UiAddLoaded);
+	}
+	function registerEvents() {
+		if (form.ExecutionContext.IsInitialLoad()) {
+		}
+	}
+	//BEGIN ON LOAD ========================================================
+	async function UiAddLoaded(executionContext) {
+	}
+	//END ON LOAD ==========================================================
+	//BEGIN ON CHANGE ======================================================
+
+	//END ON CHANGE ========================================================
+	//BEGIN PRE SEARCH =====================================================
+
+	//END PRE SEARCH =======================================================
+	//BEGIN OTHERS =========================================================
+
+	//END OTHERS ===========================================================
+	return {
+		OnLoad: onLoad
+	};
+})();
+var formContact_Quick_Create = (function () {
+	"use strict";
+	/** @type DevKitV4.FormContact_Quick_Create */
+	var form = null;
+	/** @param {any} executionContext */
+	async function onLoad(executionContext) {
+		form = new DevKitV4.FormContact_Quick_Create(executionContext);
+		registerEvents();
+		form.UiAddLoaded(UiAddLoaded);
+	}
+	function registerEvents() {
+		if (form.ExecutionContext.IsInitialLoad()) {
+		}
+	}
+	//BEGIN ON LOAD ========================================================
+	async function UiAddLoaded(executionContext) {
+	}
+	//END ON LOAD ==========================================================
+	//BEGIN ON CHANGE ======================================================
+
+	//END ON CHANGE ========================================================
+	//BEGIN PRE SEARCH =====================================================
+
+	//END PRE SEARCH =======================================================
+	//BEGIN OTHERS =========================================================
+
+	//END OTHERS ===========================================================
+	return {
+		OnLoad: onLoad
+	};
+})();
+var formContact_Information = (function () {
+	"use strict";
+	/** @type DevKitV4.FormContact_Information */
+	var form = null;
+	/** @param {any} executionContext */
+	async function onLoad(executionContext) {
+		form = new DevKitV4.FormContact_Information(executionContext);
+		registerEvents();
+		form.UiAddLoaded(UiAddLoaded);
+	}
+	function registerEvents() {
+		if (form.ExecutionContext.IsInitialLoad()) {
+		}
+	}
+	//BEGIN ON LOAD ========================================================
+	async function UiAddLoaded(executionContext) {
+	}
+	//END ON LOAD ==========================================================
+	//BEGIN ON CHANGE ======================================================
+
+	//END ON CHANGE ========================================================
+	//BEGIN PRE SEARCH =====================================================
+
+	//END PRE SEARCH =======================================================
+	//BEGIN OTHERS =========================================================
+
+	//END OTHERS ===========================================================
+	return {
+		OnLoad: onLoad
+	};
+})();
+var formInvite_Web_Form = (function () {
+	"use strict";
+	/** @type DevKitV4.FormInvite_Web_Form */
+	var form = null;
+	/** @param {any} executionContext */
+	async function onLoad(executionContext) {
+		form = new DevKitV4.FormInvite_Web_Form(executionContext);
+		registerEvents();
+		form.UiAddLoaded(UiAddLoaded);
+	}
+	function registerEvents() {
+		if (form.ExecutionContext.IsInitialLoad()) {
+		}
+	}
+	//BEGIN ON LOAD ========================================================
+	async function UiAddLoaded(executionContext) {
+	}
+	//END ON LOAD ==========================================================
+	//BEGIN ON CHANGE ======================================================
+
+	//END ON CHANGE ========================================================
+	//BEGIN PRE SEARCH =====================================================
+
+	//END PRE SEARCH =======================================================
+	//BEGIN OTHERS =========================================================
+
+	//END OTHERS ===========================================================
+	return {
+		OnLoad: onLoad
+	};
+})();
+var formPortal_Contact_Enhanced = (function () {
+	"use strict";
+	/** @type DevKitV4.FormPortal_Contact_Enhanced */
+	var form = null;
+	/** @param {any} executionContext */
+	async function onLoad(executionContext) {
+		form = new DevKitV4.FormPortal_Contact_Enhanced(executionContext);
+		registerEvents();
+		form.UiAddLoaded(UiAddLoaded);
+	}
+	function registerEvents() {
+		if (form.ExecutionContext.IsInitialLoad()) {
+		}
+	}
+	//BEGIN ON LOAD ========================================================
+	async function UiAddLoaded(executionContext) {
+	}
+	//END ON LOAD ==========================================================
+	//BEGIN ON CHANGE ======================================================
+
+	//END ON CHANGE ========================================================
+	//BEGIN PRE SEARCH =====================================================
+
+	//END PRE SEARCH =======================================================
+	//BEGIN OTHERS =========================================================
+
+	//END OTHERS ===========================================================
+	return {
+		OnLoad: onLoad
+	};
+})();
+var formProfile_Web_Form_Enhanced = (function () {
+	"use strict";
+	/** @type DevKitV4.FormProfile_Web_Form_Enhanced */
+	var form = null;
+	/** @param {any} executionContext */
+	async function onLoad(executionContext) {
+		form = new DevKitV4.FormProfile_Web_Form_Enhanced(executionContext);
+		registerEvents();
+		form.UiAddLoaded(UiAddLoaded);
+	}
+	function registerEvents() {
+		if (form.ExecutionContext.IsInitialLoad()) {
+		}
+	}
+	//BEGIN ON LOAD ========================================================
+	async function UiAddLoaded(executionContext) {
+	}
+	//END ON LOAD ==========================================================
+	//BEGIN ON CHANGE ======================================================
+
+	//END ON CHANGE ========================================================
+	//BEGIN PRE SEARCH =====================================================
+
+	//END PRE SEARCH =======================================================
+	//BEGIN OTHERS =========================================================
+
+	//END OTHERS ===========================================================
+	return {
+		OnLoad: onLoad
+	};
+})();
+var formProfile_Web_Form_Enhanced_Japanese = (function () {
+	"use strict";
+	/** @type DevKitV4.FormProfile_Web_Form_Enhanced_Japanese */
+	var form = null;
+	/** @param {any} executionContext */
+	async function onLoad(executionContext) {
+		form = new DevKitV4.FormProfile_Web_Form_Enhanced_Japanese(executionContext);
+		registerEvents();
+		form.UiAddLoaded(UiAddLoaded);
+	}
+	function registerEvents() {
+		if (form.ExecutionContext.IsInitialLoad()) {
+		}
+	}
+	//BEGIN ON LOAD ========================================================
+	async function UiAddLoaded(executionContext) {
+	}
+	//END ON LOAD ==========================================================
+	//BEGIN ON CHANGE ======================================================
+
+	//END ON CHANGE ========================================================
+	//BEGIN PRE SEARCH =====================================================
+
+	//END PRE SEARCH =======================================================
+	//BEGIN OTHERS =========================================================
+
+	//END OTHERS ===========================================================
+	return {
+		OnLoad: onLoad
+	};
+})();
