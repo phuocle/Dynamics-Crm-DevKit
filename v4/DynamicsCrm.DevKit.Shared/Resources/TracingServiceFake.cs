d88cd81cd3ab9dde41998976b14f7a2817636bd5
PhuocLe <vanphuoc@gmail.com>
2025-09-22 15:00:42 +0700

    .

--- v4/DynamicsCrm.DevKit.Shared/Resources/TracingServiceFake.cs
+++ v4/DynamicsCrm.DevKit.Shared/Resources/TracingServiceFake.cs
@@ -4,7 +4,7 @@ namespace $NameSpace$.Lib
 {
     public class TracingServiceFake : ITracingService
     {
-        public ITracingService tracingService = null;
+        public ITracingService? tracingService = null;
 
         public void Trace(string format, params object[] args)
         {
