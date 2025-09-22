e2ee83544cfcfac1d15944e5c2e8aa5f0ae4b352
PhuocLe <vanphuoc@gmail.com>
2025-09-22 16:57:23 +0700

    .

---
+++ test/4.00.00.00/TestAllProjectsV4/Dev.DevKitV4.ConsoleCore/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using System;
+
+namespace Dev.DevKitV4.ConsoleCore
+{
+    public class Program
+    {
+        static void Main()
+        {
+            CheckWhoAmI();
+            //DebugPlugin();
+            //DebugWorkflow();
+        }
+
+        private static void CheckWhoAmI()
+        {
+            var UserId = ((WhoAmIResponse)App.Service.Execute(new WhoAmIRequest())).UserId;
+            System.Console.WriteLine(UserId);
+            System.Console.ReadKey();
+        }
+
+        private static void DebugPlugin()
+        {
+            //var json = "";
+            //Helper.DebugPlugin<???>(json, App.Service);
+        }
+
+        private static void DebugWorkflow()
+        {
+            //var json = "";
+            //Helper.DebugWorkflow<???>(json, App.Service);
+        }
+    }
+}
