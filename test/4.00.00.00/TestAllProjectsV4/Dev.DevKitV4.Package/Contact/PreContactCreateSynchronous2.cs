1250f9ddd62a63ad86500dbb995864f5b2bf4253
PhuocLe <vanphuoc@gmail.com>
2025-09-23 09:26:58 +0700

    .

---
+++ test/4.00.00.00/TestAllProjectsV4/Dev.DevKitV4.Package/Contact/PreContactCreateSynchronous2.cs
@@ -0,0 +1,55 @@
+﻿using Dev.DevKitV4.Shared;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Extensions;
+using System;
+
+namespace Dev.DevKitV4.Package.Contact
+{
+    [CrmPluginRegistration("Create", "contact", StageEnum.PreOperation, ExecutionModeEnum.Synchronous, "", "Dev.DevKitV4.Package.Contact.PreContactCreateSynchronous2", 2, IsolationModeEnum.Sandbox, PluginType = PluginType.Plugin)]
+    public class PreContactCreateSynchronous2 : IPlugin
+    {
+        /*
+        InputParameters:
+            Target                             Microsoft.Xrm.Sdk.Entity - require
+            SuppressDuplicateDetection         System.Boolean
+            CalculateMatchCodeSynchronously    System.Boolean
+            SolutionUniqueName                 System.String
+            MaintainLegacyAppServerBehavior    System.Boolean
+            ReturnRowVersion                   System.Boolean
+        OutputParameters:
+            id                                 System.Guid - require
+        */
+
+        //private readonly string unSecureConfiguration = null;
+        //private readonly string secureConfiguration = null;
+        //public PreContactCreateSynchronous2(string unSecureConfiguration, string secureConfiguration)
+        //{
+        //    this.unSecureConfiguration = unSecureConfiguration;
+        //    this.secureConfiguration = secureConfiguration;
+        //}
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var tracing = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            if (!int.Equals(context.Stage, (int)StageEnum.PreOperation)) throw new InvalidPluginExecutionException("Stage does not equals PreOperation");
+            if (!string.Equals(context.MessageName, "Create", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("MessageName does not equals Create");
+            if (!string.Equals(context.PrimaryEntityName, "contact", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("PrimaryEntityName does not equals contact");
+            if (!int.Equals(context.Mode, (int)ExecutionModeEnum.Synchronous)) throw new InvalidPluginExecutionException("Execution does not equals Synchronous");
+            var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var serviceAdmin = serviceFactory.CreateOrganizationService(null);
+            var service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            tracing?.DebugContext(context);
+
+            ExecutePlugin(context, serviceFactory, serviceAdmin, service, tracing);
+        }
+
+        private void ExecutePlugin(IPluginExecutionContext context, IOrganizationServiceFactory serviceFactory, IOrganizationService serviceAdmin, IOrganizationService service, ITracingService tracing)
+        {
+            var targetEntity = context.InputParameterOrDefault<Entity>("Target");
+            //YOUR PLUGIN-CODE GO HERE
+            targetEntity["jobtitle"] = "Modified by PreContactCreateSynchronous2";
+        }
+    }
+}
