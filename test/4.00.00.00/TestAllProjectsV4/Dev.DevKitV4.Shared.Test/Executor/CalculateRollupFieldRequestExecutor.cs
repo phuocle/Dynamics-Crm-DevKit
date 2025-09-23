f8c171f4bb3d9b16433b8066f68cbc51ae84b05c
PhuocLe <vanphuoc@gmail.com>
2025-09-23 09:56:41 +0700

    .

---
+++ test/4.00.00.00/TestAllProjectsV4/Dev.DevKitV4.Shared.Test/Executor/CalculateRollupFieldRequestExecutor.cs
@@ -0,0 +1,26 @@
+﻿using FakeXrmEasy.Abstractions;
+using FakeXrmEasy.Abstractions.FakeMessageExecutors;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using System;
+
+namespace Dev.DevKitV4.Shared.Test
+{
+    public class CalculateRollupFieldRequestExecutor : IFakeMessageExecutor
+    {
+        public bool CanExecute(OrganizationRequest request)
+        {
+            return request is CalculateRollupFieldRequest;
+        }
+
+        public OrganizationResponse Execute(OrganizationRequest request, IXrmFakedContext ctx)
+        {
+            return new CalculateRollupFieldResponse();
+        }
+
+        public Type GetResponsibleRequestType()
+        {
+            return typeof(CalculateRollupFieldRequest);
+        }
+    }
+}
