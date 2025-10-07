824aaf917e933f9783cc61f7fb7ea6358ecf684e
PhuocLe <vanphuoc@gmail.com>
2025-10-07 08:30:37 +0700

    .

--- test/4.00.00.00/TestAllProjectsV4/Dev.DevKitV4.Shared/Lib/PluginCore.cs
+++ test/4.00.00.00/TestAllProjectsV4/Dev.DevKitV4.Shared/Lib/PluginCore.cs
@@ -120,4 +120,39 @@ namespace Dev.DevKitV4.Shared
         public PluginType PluginType { get; set; }
         public string DataSource { get; set; }
     }
+
+    public enum ManagedIdentityCredentialSource
+    {
+        EntraIdApplication = 2,
+        UserAssignedManagedIdentity = 3
+    }
+
+    public enum ManagedIdentitySubjectScope
+    {
+        Environment = 1,
+        Organization = 2
+    }
+
+    public enum AzureCloudEnvironment
+    {
+        Public,
+        GCC,
+        GCCHigh,
+        China,
+        USNat,
+        USSec
+    }
+
+    [DebuggerNonUserCode()]
+    [AttributeUsage(AttributeTargets.Assembly, Inherited = false, AllowMultiple = false)]
+    public class AssemblyManagedIdentityAttribute : Attribute
+    {
+        public string ApplicationId { get; set; }
+        public string TenantId { get; set; }
+        public ManagedIdentityCredentialSource CredentialSource { get; set; } = ManagedIdentityCredentialSource.EntraIdApplication;
+        public ManagedIdentitySubjectScope SubjectScope { get; set; } = ManagedIdentitySubjectScope.Environment;
+        public string CertificatePath { get; set; } = string.Empty;
+        public string CertificatePassword { get; set; } = string.Empty;
+        public AzureCloudEnvironment CloudEnvironment { get; set; } = AzureCloudEnvironment.Public;
+    }
 }
