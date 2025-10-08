a788a0d8218419c8d6bafe4dc7d16920a3bbffa8
PhuocLe <vanphuoc@gmail.com>
2025-10-08 15:46:32 +0700

    .

---
+++ test/4.00.00.00/TestAllProjectsV4/Dev.DevKitV4.Server.2/ManagedIdentity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+[assembly: DynamicsCrmDevkitAssembly(
+    IsolationMode = IsolationModeEnum.Sandbox,
+    SourceType = SourceTypeEnum.Database,
+    TenantId = "49528483-b79b-4b88-b86e-7d882ba68911",
+    ApplicationId = "c7477d59-0d76-47bc-a235-0d31b89831ff",
+    CertificatePath = "ManagedIdentity.pfx",
+    CertificatePassword = "CertificatePassword123!",
+    CredentialSource = CredentialSource.IsManaged,
+    SubjectScope = SubjectScope.EnvironmentScope
+)]
