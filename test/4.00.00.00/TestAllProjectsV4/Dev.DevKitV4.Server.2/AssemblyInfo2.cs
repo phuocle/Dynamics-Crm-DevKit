6e2d73df7efe805a9c99ec4cbd03ffa235ffd128
PhuocLe <vanphuoc@gmail.com>
2025-10-09 16:39:13 +0700

    .

--- test/4.00.00.00/TestAllProjectsV4/Dev.DevKitV4.Server.2/AssemblyInfo2.cs
+++ test/4.00.00.00/TestAllProjectsV4/Dev.DevKitV4.Server.2/AssemblyInfo2.cs
@@ -5,7 +5,7 @@ using System;
     IsolationMode = IsolationModeEnum.Sandbox,
     SourceType = SourceTypeEnum.Database,
     TenantId = "49528483-b79b-4b88-b86e-7d882ba68911",
-    ApplicationId = "8abd7e92-86e1-4660-8407-0170eb81500c",
+    ApplicationId = "c60f6fe5-92a2-485c-9149-f84e8f73c105",
     CertificatePath = "ManagedIdentity.pfx",
     CertificatePassword = "!CertificatePassword123!",
     CredentialSource = CredentialSource.IsManaged,
