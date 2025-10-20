fe5af42b0ff63f227e61b7f94deb8bf5a7fdd69f
PhuocLe <vanphuoc@gmail.com>
2025-10-20 10:07:28 +0700

    .

--- test/4.00.00.00/TestAllProjectsV4/Dev.DevKitV4.Package.2/PostAccountUpdateAsynchronous.cs
+++ test/4.00.00.00/TestAllProjectsV4/Dev.DevKitV4.Package.2/PostAccountUpdateAsynchronous.cs
@@ -7,7 +7,7 @@ using System.Runtime.Remoting.Services;
 
 namespace Dev.DevKitV4.Package._2
 {
-    [CrmPluginRegistration("Update", "account", StageEnum.PostOperation, ExecutionModeEnum.Asynchronous, "name", "Dev.DevKitV4.Package._2.PostAccountUpdateAsynchronous", 1, IsolationModeEnum.Sandbox, PluginType = PluginType.Plugin, DeleteAsyncOperation = true, Image1Name = "PreImage", Image1Alias = "PreImage", Image1Type = ImageTypeEnum.PreImage, Image1Attributes = "*", Image2Name = "PostImage", Image2Alias = "PostImage", Image2Type = ImageTypeEnum.PostImage, Image2Attributes = "*", SecureConfiguration = "SecureConfiguration", UnSecureConfiguration = "UnSecureConfiguration")]
+    [CrmPluginRegistration("Update", "account", StageEnum.PostOperation, ExecutionModeEnum.Asynchronous, "name", "Dev.DevKitV4.Package._2.PostAccountUpdateAsynchronous", 1, IsolationModeEnum.Sandbox, PluginType = PluginType.Plugin, DeleteAsyncOperation = true, Image1Name = "PreImage", Image1Alias = "PreImage", Image1Type = ImageTypeEnum.PreImage, Image1Attributes = "*", Image2Name = "PostImage", Image2Alias = "PostImage", Image2Type = ImageTypeEnum.PostImage, Image2Attributes = "*", SecureConfiguration = "SecureConfiguration2", UnSecureConfiguration = "UnSecureConfiguration")]
     public class PostAccountUpdateAsynchronous : IPlugin
     {
         /*
