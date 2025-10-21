ac8de7c5d0efc209697eba1b19489d238480e181
PhuocLe <vanphuoc@gmail.com>
2025-10-21 07:43:55 +0700

    .

---
+++ test/4.00.00.00/TestAllProjectsV4/Dev.DevKitV4.Server.2/AssemblyInfo2.cs
@@ -0,0 +1,8 @@
+﻿using Dev.DevKitV4.Shared;
+
+[assembly: DynamcisCrmDevKitPluginManagedIdentityAssembly(
+    TenantId = "49528483-b79b-4b88-b86e-7d882ba68911",
+    ApplicationIds = "a12f1dfe-d2c1-4fa1-9061-7c35d8ce0ad4,febff232-1ff5-4a5b-a34e-48624fa477bc",
+    CertificateFile = "ManagedIdentity.pfx",
+    CertificatePassword = "!123@abCDef"
+)]
