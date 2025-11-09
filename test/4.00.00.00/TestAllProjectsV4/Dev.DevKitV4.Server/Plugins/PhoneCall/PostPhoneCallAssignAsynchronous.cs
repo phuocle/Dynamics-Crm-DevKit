d98162cca9f997c073709ce2fb4471f1c4a9ce7e
PhuocLe <vanphuoc@gmail.com>
2025-11-09 13:25:36 +0700

    .

--- test/4.00.00.00/TestAllProjectsV4/Dev.DevKitV4.Server/Plugins/PhoneCall/PostPhoneCallAssignAsynchronous.cs
+++ test/4.00.00.00/TestAllProjectsV4/Dev.DevKitV4.Server/Plugins/PhoneCall/PostPhoneCallAssignAsynchronous.cs
@@ -3,23 +3,21 @@ using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Extensions;
 using System;
 
-namespace Dev.DevKitV4.Server.Plugins.Email
+namespace Dev.DevKitV4.Server.Plugins.PhoneCall
 {
-    [CrmPluginRegistration("Send", "email", StageEnum.PostOperation, ExecutionModeEnum.Asynchronous, "", "Dev.DevKitV4.Server.Plugins.Email.PostEmailSendAsynchronous", 1, IsolationModeEnum.Sandbox, PluginType = PluginType.Plugin, DeleteAsyncOperation = true, Image1Name = "PreImage", Image1Alias = "PreImage", Image1Type = ImageTypeEnum.PreImage, Image1Attributes = "*", Image2Name = "PostImage", Image2Alias = "PostImage", Image2Type = ImageTypeEnum.PostImage, Image2Attributes = "*", SecureConfiguration = "ABCD", UnSecureConfiguration = "DEF")]
-    public class PostEmailSendAsynchronous : IPlugin
+    [CrmPluginRegistration("Assign", "phonecall", StageEnum.PostOperation, ExecutionModeEnum.Asynchronous, "", "Dev.DevKitV4.Server.Plugins.PhoneCall.PostPhoneCallAssignAsynchronous", 1, IsolationModeEnum.Sandbox, PluginType = PluginType.Plugin, DeleteAsyncOperation = true, Image1Name = "PreImage", Image1Alias = "PreImage", Image1Type = ImageTypeEnum.PreImage, Image1Attributes = "*", Image2Name = "PostImage", Image2Alias = "PostImage", Image2Type = ImageTypeEnum.PostImage, Image2Attributes = "*")]
+    public class PostPhoneCallAssignAsynchronous : IPlugin
     {
         /*
         InputParameters:
-            EmailId          System.Guid - require
-            IssueSend        System.Boolean - require
-            TrackingToken    System.String
+            Target      Microsoft.Xrm.Sdk.EntityReference - require
+            Assignee    Microsoft.Xrm.Sdk.EntityReference - require
         OutputParameters:
-            Subject          System.String - require
         */
 
         //private readonly string unSecureConfiguration = null;
         //private readonly string secureConfiguration = null;
-        //public PostEmailSendAsynchronous(string unSecureConfiguration, string secureConfiguration)
+        //public PostPhoneCallAssignAsynchronous(string unSecureConfiguration, string secureConfiguration)
         //{
         //    this.unSecureConfiguration = unSecureConfiguration;
         //    this.secureConfiguration = secureConfiguration;
@@ -30,14 +28,14 @@ namespace Dev.DevKitV4.Server.Plugins.Email
             var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
             var tracing = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
             if (!int.Equals(context.Stage, (int)StageEnum.PostOperation)) throw new InvalidPluginExecutionException("Stage does not equals PostOperation");
-            if (!string.Equals(context.MessageName, "Send", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("MessageName does not equals Send");
-            if (!string.Equals(context.PrimaryEntityName, "email", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("PrimaryEntityName does not equals email");
+            if (!string.Equals(context.MessageName, "Assign", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("MessageName does not equals Assign");
+            if (!string.Equals(context.PrimaryEntityName, "phonecall", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("PrimaryEntityName does not equals phonecall");
             if (!int.Equals(context.Mode, (int)ExecutionModeEnum.Asynchronous)) throw new InvalidPluginExecutionException("Execution does not equals Asynchronous");
             var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
             var serviceAdmin = serviceFactory.CreateOrganizationService(null);
             var service = serviceFactory.CreateOrganizationService(context.UserId);
 
-            //tracing?.DebugContext(context);
+            tracing?.DebugContext(context);
 
             ExecutePlugin(context, serviceFactory, serviceAdmin, service, tracing);
         }
--- test/4.00.00.00/TestAllProjectsV4/Dev.DevKitV4.Server/Plugins/Task/PostTaskCreateMultipleAsynchronous.cs
+++ test/4.00.00.00/TestAllProjectsV4/Dev.DevKitV4.Server/Plugins/Task/PostTaskCreateMultipleAsynchronous.cs
@@ -3,23 +3,21 @@ using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Extensions;
 using System;
 
-namespace Dev.DevKitV4.Server.Plugins.Email
+namespace Dev.DevKitV4.Server.Plugins.Task
 {
-    [CrmPluginRegistration("Send", "email", StageEnum.PostOperation, ExecutionModeEnum.Asynchronous, "", "Dev.DevKitV4.Server.Plugins.Email.PostEmailSendAsynchronous", 1, IsolationModeEnum.Sandbox, PluginType = PluginType.Plugin, DeleteAsyncOperation = true, Image1Name = "PreImage", Image1Alias = "PreImage", Image1Type = ImageTypeEnum.PreImage, Image1Attributes = "*", Image2Name = "PostImage", Image2Alias = "PostImage", Image2Type = ImageTypeEnum.PostImage, Image2Attributes = "*", SecureConfiguration = "ABCD", UnSecureConfiguration = "DEF")]
-    public class PostEmailSendAsynchronous : IPlugin
+    [CrmPluginRegistration("CreateMultiple", "task", StageEnum.PostOperation, ExecutionModeEnum.Asynchronous, "", "Dev.DevKitV4.Server.Plugins.Task.PostTaskCreateMultipleAsynchronous", 1, IsolationModeEnum.Sandbox, PluginType = PluginType.Plugin, DeleteAsyncOperation = true, Image1Name = "PostImage", Image1Alias = "PostImage", Image1Type = ImageTypeEnum.PostImage, Image1Attributes = "*")]
+    public class PostTaskCreateMultipleAsynchronous : IPlugin
     {
         /*
         InputParameters:
-            EmailId          System.Guid - require
-            IssueSend        System.Boolean - require
-            TrackingToken    System.String
+            Targets    Microsoft.Xrm.Sdk.EntityCollection - require
         OutputParameters:
-            Subject          System.String - require
+            Ids        System.Guid[] - require
         */
 
         //private readonly string unSecureConfiguration = null;
         //private readonly string secureConfiguration = null;
-        //public PostEmailSendAsynchronous(string unSecureConfiguration, string secureConfiguration)
+        //public PostTaskCreateMultipleAsynchronous(string unSecureConfiguration, string secureConfiguration)
         //{
         //    this.unSecureConfiguration = unSecureConfiguration;
         //    this.secureConfiguration = secureConfiguration;
@@ -30,22 +28,21 @@ namespace Dev.DevKitV4.Server.Plugins.Email
             var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
             var tracing = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
             if (!int.Equals(context.Stage, (int)StageEnum.PostOperation)) throw new InvalidPluginExecutionException("Stage does not equals PostOperation");
-            if (!string.Equals(context.MessageName, "Send", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("MessageName does not equals Send");
-            if (!string.Equals(context.PrimaryEntityName, "email", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("PrimaryEntityName does not equals email");
+            if (!string.Equals(context.MessageName, "CreateMultiple", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("MessageName does not equals CreateMultiple");
+            if (!string.Equals(context.PrimaryEntityName, "task", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("PrimaryEntityName does not equals task");
             if (!int.Equals(context.Mode, (int)ExecutionModeEnum.Asynchronous)) throw new InvalidPluginExecutionException("Execution does not equals Asynchronous");
             var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
             var serviceAdmin = serviceFactory.CreateOrganizationService(null);
             var service = serviceFactory.CreateOrganizationService(context.UserId);
 
-            //tracing?.DebugContext(context);
+            tracing?.DebugContext(context);
 
             ExecutePlugin(context, serviceFactory, serviceAdmin, service, tracing);
         }
 
         private void ExecutePlugin(IPluginExecutionContext context, IOrganizationServiceFactory serviceFactory, IOrganizationService serviceAdmin, IOrganizationService service, ITracingService tracing)
         {
-            //var ??? = context.InputParameterOrDefault<???>("???");
-            context.PreEntityImages.TryGetValue("PreImage", out Entity preEntity);
+            var targetEntities = context.InputParameterOrDefault<EntityCollection>("Targets");
             context.PostEntityImages.TryGetValue("PostImage", out Entity postEntity);
             //YOUR PLUGIN-CODE GO HERE
 
