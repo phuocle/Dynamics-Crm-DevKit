98b814ff34fcaeb886c4490711bb3ebc6ffbcc34
PhuocLe <vanphuoc@gmail.com>
2025-12-15 13:20:01 +0700

    .

---
+++ test/v4/TestAddCrmPluginRegistration/1.before/AccountPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("AccountPlugin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("2065a8ea-e72f-4764-8198-c2df2a9a5a5a")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
