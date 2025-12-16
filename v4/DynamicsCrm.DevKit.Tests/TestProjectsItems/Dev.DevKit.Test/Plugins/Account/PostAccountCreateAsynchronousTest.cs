5231ac81a3a5853007b45a3a98ccdeefe7eb6ff5
PhuocLe <vanphuoc@gmail.com>
2025-12-16 14:23:27 +0700

    .

---
+++ v4/DynamicsCrm.DevKit.Tests/TestProjectsItems/Dev.DevKit.Test/Plugins/Account/PostAccountCreateAsynchronousTest.cs
@@ -0,0 +1,68 @@
+﻿using Dev.DevKit.Shared;
+using Dev.DevKit.Shared.Test;
+using FakeXrmEasy.Plugins;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk;
+using System.Linq;
+
+namespace Dev.DevKit.Test.Plugins.Account
+{
+    [TestClass]
+    public class PostAccountCreateAsynchronousTest : FakeXrmEasyTestBase
+    {
+        private const StageEnum PLUGIN_STAGE = StageEnum.PostOperation;
+        private const string PLUGIN_MESSAGE = "???";
+        private const string PLUGIN_ENTITY_LOGICAL_NAME = "???";
+        private const ExecutionModeEnum PLUGIN_EXECUTION_MODE = ExecutionModeEnum.Synchronous;
+
+        [TestMethod]
+        public void PostAccountCreateAsynchronousTest_00()
+        {
+            var pluginContext = _context.GetDefaultPluginContext();
+
+            Assert.ThrowsExactly<InvalidPluginExecutionException>(() =>
+            {
+                _context.ExecutePluginWith <???> (pluginContext);
+            }, $"Stage does not equals {PLUGIN_STAGE}");
+
+            pluginContext.Stage = (int)PLUGIN_STAGE;
+            pluginContext.MessageName = string.Empty;
+            Assert.ThrowsExactly<InvalidPluginExecutionException>(() =>
+            {
+                _context.ExecutePluginWith <???> (pluginContext);
+            }, $"MessageName does not equals {PLUGIN_MESSAGE}");
+
+            pluginContext.MessageName = PLUGIN_MESSAGE;
+            pluginContext.PrimaryEntityName = string.Empty;
+            Assert.ThrowsExactly<InvalidPluginExecutionException>(() =>
+            {
+                _context.ExecutePluginWith <???> (pluginContext);
+            }, $"PrimaryEntityName does not equals {PLUGIN_ENTITY_LOGICAL_NAME}");
+
+            pluginContext.PrimaryEntityName = PLUGIN_ENTITY_LOGICAL_NAME;
+            pluginContext.Mode = -1;
+            Assert.ThrowsExactly<InvalidPluginExecutionException>(() =>
+            {
+                _context.ExecutePluginWith <???> (pluginContext);
+            }, $"Execution does not equals {PLUGIN_EXECUTION_MODE}");
+
+            pluginContext.Mode = (int)PLUGIN_EXECUTION_MODE;
+            _context.ExecutePluginWith <???> (pluginContext);
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void PostAccountCreateAsynchronousTest_01()
+        {
+            ////setup
+            //var json = @"";
+            //var remote = TestHelper.DeserializeRemoteExecutionContext(json);
+            //var pluginContext = _context.GetDefaultPluginContext();
+            //pluginContext.SetXrmFakedContextPlugin(remote);
+            ////run
+            //_context.ExecutePluginWith<???> (pluginContext);
+            ////result
+            Assert.IsTrue(true);
+        }
+    }
+}
