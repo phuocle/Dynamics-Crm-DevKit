9995c5c0eab185288ee38188c4cfa3b37515f160
PhuocLe <vanphuoc@gmail.com>
2025-12-27 17:57:01 +0700

    .

--- v4/DynamicsCrm.DevKit.Tests/TestDevKitJs/lib/devkit.js
+++ v4/DynamicsCrm.DevKit.Tests/TestDevKitJs/lib/devkit.js
@@ -1061,4 +1061,3 @@ var OptionSet;
     OptionSet.TabDisplayState = Object.freeze({ Expanded: 'expanded', Collapsed: 'collapsed' });
     OptionSet.TimerState = Object.freeze({ NotSet: 1, InProgress: 2, Warning: 3, Violated: 4, Success: 5, Expired: 6, Canceled: 7, Paused: 8 });
 })(OptionSet || (OptionSet = {}));
-export { devKit, OptionSet };
