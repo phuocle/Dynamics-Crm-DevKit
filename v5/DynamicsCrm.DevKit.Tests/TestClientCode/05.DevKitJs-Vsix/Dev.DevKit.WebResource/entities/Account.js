0ab3d91e102999e5e67872f83fed16b6d1da6959
PhuocLe <vanphuoc@gmail.com>
2026-01-07 16:41:54 +0700

    .

--- v5/DynamicsCrm.DevKit.Tests/TestClientCode/05.DevKitJs-Vsix/Dev.DevKit.WebResource/entities/Account.js
+++ v5/DynamicsCrm.DevKit.Tests/TestClientCode/05.DevKitJs-Vsix/Dev.DevKit.WebResource/entities/Account.js
@@ -1,163 +1,172 @@
 ﻿//@ts-check
 ///<reference path="Account.d.ts" />
 "use strict";
-var formAccount = (function () {
-	"use strict";
-	/** @type {DevKit.FormAccount} */
-	let form;
-	/** @param {any} executionContext */
-	async function onLoad(executionContext) {
-		form = new DevKit.FormAccount(executionContext);
-		registerEvents();
-		form.UiAddLoaded(UiAddLoaded);
-	}
-	function registerEvents() {
-		if (form.ExecutionContext.IsInitialLoad()) {
-		}
-	}
-	//BEGIN ON LOAD ========================================================
-	/** @param {any} executionContext */
-	async function UiAddLoaded(executionContext) {
-	}
-	//END ON LOAD ==========================================================
-	//BEGIN ON CHANGE ======================================================
 
-	//END ON CHANGE ========================================================
-	//BEGIN PRE SEARCH =====================================================
+//var formAccount = (function () {
+//	"use strict";
+//	/** @type {DevKit.FormAccount} */
+//	let form;
+//	/** @param {any} executionContext */
+//	async function onLoad(executionContext) {
+//		form = new DevKit.FormAccount(executionContext);
+//		registerEvents();
+//		form.UiAddLoaded(UiAddLoaded);
+//	}
+//	function registerEvents() {
+//		if (form.ExecutionContext.IsInitialLoad()) {
+//		}
+//	}
+//	//BEGIN ON LOAD ========================================================
+//	/** @param {any} executionContext */
+//	async function UiAddLoaded(executionContext) {
+//	}
+//	//END ON LOAD ==========================================================
+//	//BEGIN ON CHANGE ======================================================
+//
+//	//END ON CHANGE ========================================================
+//	//BEGIN PRE SEARCH =====================================================
+//
+//	//END PRE SEARCH =======================================================
+//	//BEGIN OTHERS =========================================================
+//
+//	//END OTHERS ===========================================================
+//	return {
+//		OnLoad: onLoad
+//	};
+//})();
 
-	//END PRE SEARCH =======================================================
-	//BEGIN OTHERS =========================================================
 
-	//END OTHERS ===========================================================
-	return {
-		OnLoad: onLoad
-	};
-})();
-var formAccount_DevKitV4 = (function () {
-	"use strict";
-	/** @type {DevKit.FormAccount_DevKitV4} */
-	let form;
-	/** @param {any} executionContext */
-	async function onLoad(executionContext) {
-		form = new DevKit.FormAccount_DevKitV4(executionContext);
-		registerEvents();
-		form.UiAddLoaded(UiAddLoaded);
-	}
-	function registerEvents() {
-		if (form.ExecutionContext.IsInitialLoad()) {
-		}
-	}
-	//BEGIN ON LOAD ========================================================
-	/** @param {any} executionContext */
-	async function UiAddLoaded(executionContext) {
-	}
-	//END ON LOAD ==========================================================
-	//BEGIN ON CHANGE ======================================================
+//var formAccount_DevKitV4 = (function () {
+//	"use strict";
+//	/** @type {DevKit.FormAccount_DevKitV4} */
+//	let form;
+//	/** @param {any} executionContext */
+//	async function onLoad(executionContext) {
+//		form = new DevKit.FormAccount_DevKitV4(executionContext);
+//		registerEvents();
+//		form.UiAddLoaded(UiAddLoaded);
+//	}
+//	function registerEvents() {
+//		if (form.ExecutionContext.IsInitialLoad()) {
+//		}
+//	}
+//	//BEGIN ON LOAD ========================================================
+//	/** @param {any} executionContext */
+//	async function UiAddLoaded(executionContext) {
+//	}
+//	//END ON LOAD ==========================================================
+//	//BEGIN ON CHANGE ======================================================
+//
+//	//END ON CHANGE ========================================================
+//	//BEGIN PRE SEARCH =====================================================
+//
+//	//END PRE SEARCH =======================================================
+//	//BEGIN OTHERS =========================================================
+//
+//	//END OTHERS ===========================================================
+//	return {
+//		OnLoad: onLoad
+//	};
+//})();
 
-	//END ON CHANGE ========================================================
-	//BEGIN PRE SEARCH =====================================================
 
-	//END PRE SEARCH =======================================================
-	//BEGIN OTHERS =========================================================
+//var formAccount_for_Interactive_experience = (function () {
+//	"use strict";
+//	/** @type {DevKit.FormAccount_for_Interactive_experience} */
+//	let form;
+//	/** @param {any} executionContext */
+//	async function onLoad(executionContext) {
+//		form = new DevKit.FormAccount_for_Interactive_experience(executionContext);
+//		registerEvents();
+//		form.UiAddLoaded(UiAddLoaded);
+//	}
+//	function registerEvents() {
+//		if (form.ExecutionContext.IsInitialLoad()) {
+//		}
+//	}
+//	//BEGIN ON LOAD ========================================================
+//	/** @param {any} executionContext */
+//	async function UiAddLoaded(executionContext) {
+//	}
+//	//END ON LOAD ==========================================================
+//	//BEGIN ON CHANGE ======================================================
+//
+//	//END ON CHANGE ========================================================
+//	//BEGIN PRE SEARCH =====================================================
+//
+//	//END PRE SEARCH =======================================================
+//	//BEGIN OTHERS =========================================================
+//
+//	//END OTHERS ===========================================================
+//	return {
+//		OnLoad: onLoad
+//	};
+//})();
 
-	//END OTHERS ===========================================================
-	return {
-		OnLoad: onLoad
-	};
-})();
-var formAccount_for_Interactive_experience = (function () {
-	"use strict";
-	/** @type {DevKit.FormAccount_for_Interactive_experience} */
-	let form;
-	/** @param {any} executionContext */
-	async function onLoad(executionContext) {
-		form = new DevKit.FormAccount_for_Interactive_experience(executionContext);
-		registerEvents();
-		form.UiAddLoaded(UiAddLoaded);
-	}
-	function registerEvents() {
-		if (form.ExecutionContext.IsInitialLoad()) {
-		}
-	}
-	//BEGIN ON LOAD ========================================================
-	/** @param {any} executionContext */
-	async function UiAddLoaded(executionContext) {
-	}
-	//END ON LOAD ==========================================================
-	//BEGIN ON CHANGE ======================================================
 
-	//END ON CHANGE ========================================================
-	//BEGIN PRE SEARCH =====================================================
+//var formAccount_Quick_Create = (function () {
+//	"use strict";
+//	/** @type {DevKit.FormAccount_Quick_Create} */
+//	let form;
+//	/** @param {any} executionContext */
+//	async function onLoad(executionContext) {
+//		form = new DevKit.FormAccount_Quick_Create(executionContext);
+//		registerEvents();
+//		form.UiAddLoaded(UiAddLoaded);
+//	}
+//	function registerEvents() {
+//		if (form.ExecutionContext.IsInitialLoad()) {
+//		}
+//	}
+//	//BEGIN ON LOAD ========================================================
+//	/** @param {any} executionContext */
+//	async function UiAddLoaded(executionContext) {
+//	}
+//	//END ON LOAD ==========================================================
+//	//BEGIN ON CHANGE ======================================================
+//
+//	//END ON CHANGE ========================================================
+//	//BEGIN PRE SEARCH =====================================================
+//
+//	//END PRE SEARCH =======================================================
+//	//BEGIN OTHERS =========================================================
+//
+//	//END OTHERS ===========================================================
+//	return {
+//		OnLoad: onLoad
+//	};
+//})();
 
-	//END PRE SEARCH =======================================================
-	//BEGIN OTHERS =========================================================
 
-	//END OTHERS ===========================================================
-	return {
-		OnLoad: onLoad
-	};
-})();
-var formAccount_Quick_Create = (function () {
-	"use strict";
-	/** @type {DevKit.FormAccount_Quick_Create} */
-	let form;
-	/** @param {any} executionContext */
-	async function onLoad(executionContext) {
-		form = new DevKit.FormAccount_Quick_Create(executionContext);
-		registerEvents();
-		form.UiAddLoaded(UiAddLoaded);
-	}
-	function registerEvents() {
-		if (form.ExecutionContext.IsInitialLoad()) {
-		}
-	}
-	//BEGIN ON LOAD ========================================================
-	/** @param {any} executionContext */
-	async function UiAddLoaded(executionContext) {
-	}
-	//END ON LOAD ==========================================================
-	//BEGIN ON CHANGE ======================================================
-
-	//END ON CHANGE ========================================================
-	//BEGIN PRE SEARCH =====================================================
-
-	//END PRE SEARCH =======================================================
-	//BEGIN OTHERS =========================================================
-
-	//END OTHERS ===========================================================
-	return {
-		OnLoad: onLoad
-	};
-})();
-var formAccount_Information = (function () {
-	"use strict";
-	/** @type {DevKit.FormAccount_Information} */
-	let form;
-	/** @param {any} executionContext */
-	async function onLoad(executionContext) {
-		form = new DevKit.FormAccount_Information(executionContext);
-		registerEvents();
-		form.UiAddLoaded(UiAddLoaded);
-	}
-	function registerEvents() {
-		if (form.ExecutionContext.IsInitialLoad()) {
-		}
-	}
-	//BEGIN ON LOAD ========================================================
-	/** @param {any} executionContext */
-	async function UiAddLoaded(executionContext) {
-	}
-	//END ON LOAD ==========================================================
-	//BEGIN ON CHANGE ======================================================
-
-	//END ON CHANGE ========================================================
-	//BEGIN PRE SEARCH =====================================================
-
-	//END PRE SEARCH =======================================================
-	//BEGIN OTHERS =========================================================
-
-	//END OTHERS ===========================================================
-	return {
-		OnLoad: onLoad
-	};
-})();
+//var formAccount_Information = (function () {
+//	"use strict";
+//	/** @type {DevKit.FormAccount_Information} */
+//	let form;
+//	/** @param {any} executionContext */
+//	async function onLoad(executionContext) {
+//		form = new DevKit.FormAccount_Information(executionContext);
+//		registerEvents();
+//		form.UiAddLoaded(UiAddLoaded);
+//	}
+//	function registerEvents() {
+//		if (form.ExecutionContext.IsInitialLoad()) {
+//		}
+//	}
+//	//BEGIN ON LOAD ========================================================
+//	/** @param {any} executionContext */
+//	async function UiAddLoaded(executionContext) {
+//	}
+//	//END ON LOAD ==========================================================
+//	//BEGIN ON CHANGE ======================================================
+//
+//	//END ON CHANGE ========================================================
+//	//BEGIN PRE SEARCH =====================================================
+//
+//	//END PRE SEARCH =======================================================
+//	//BEGIN OTHERS =========================================================
+//
+//	//END OTHERS ===========================================================
+//	return {
+//		OnLoad: onLoad
+//	};
+//})();
