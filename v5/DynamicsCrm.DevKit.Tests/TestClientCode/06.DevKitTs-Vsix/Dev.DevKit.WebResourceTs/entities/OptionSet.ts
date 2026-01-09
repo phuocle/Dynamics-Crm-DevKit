9a77057aba3ce1931b4a92e3e5899515126099e0
PhuocLe <vanphuoc@gmail.com>
2026-01-09 16:25:51 +0700

    .

--- v5/DynamicsCrm.DevKit.Tests/TestClientCode/06.DevKitTs-Vsix/Dev.DevKit.WebResourceTs/entities/OptionSet.ts
+++ v5/DynamicsCrm.DevKit.Tests/TestClientCode/06.DevKitTs-Vsix/Dev.DevKit.WebResourceTs/entities/OptionSet.ts
@@ -588,6 +588,7 @@ const Account = {
 	/** Rollup State */
 	RollupState: { NotCalculated: 0, Calculated: 1, OverflowError: 2, OtherError: 3, RetryLimitExceeded: 4, HierarchicalRecursionLimitReached: 5, LoopDetected: 6 }
 } as const;
+
 /** Contact entity OptionSets */
 const Contact = {
 	/** Role */
@@ -648,6 +649,18 @@ const Contact = {
 	RollupState: { NotCalculated: 0, Calculated: 1, OverflowError: 2, OtherError: 3, RetryLimitExceeded: 4, HierarchicalRecursionLimitReached: 5, LoopDetected: 6 }
 } as const;
 
+/** Team entity OptionSets */
+const Team = {
+	/** Membership Type */
+	MembershipType: { Guests: 3, Members: 1, Members_and_guests: 0, Owners: 2 },
+	/** RegardingObjectTypeCode */
+	RegardingObjectTypeCode: {},
+	/** Team Type */
+	TeamType: { Access: 1, Office_Group: 3, Owner: 0, Security_Group: 2 },
+	/** Rollup State */
+	RollupState: { NotCalculated: 0, Calculated: 1, OverflowError: 2, OtherError: 3, RetryLimitExceeded: 4, HierarchicalRecursionLimitReached: 5, LoopDetected: 6 }
+} as const;
+
 // ============================================================================
 // Export combined OptionSet
 // ============================================================================
@@ -690,6 +703,7 @@ export const OptionSet = {
 	TimerState,
 	// Entity OptionSets
 	Account,
-	Contact
+	Contact,
+	Team
 } as const;
 
