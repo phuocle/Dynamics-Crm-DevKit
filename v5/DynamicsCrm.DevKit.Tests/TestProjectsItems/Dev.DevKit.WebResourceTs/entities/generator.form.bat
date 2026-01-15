7f43743fb1bcab9e49007e4b1671eee241379408
PhuocLe <vanphuoc@gmail.com>
2026-01-16 06:20:43 +0700

    .

--- v5/DynamicsCrm.DevKit.Tests/TestProjectsItems/Dev.DevKit.WebResourceTs/entities/generator.form.bat
+++ v5/DynamicsCrm.DevKit.Tests/TestProjectsItems/Dev.DevKit.WebResourceTs/entities/generator.form.bat
@@ -1,4 +1,2 @@
 @echo off
-for /f "delims=" %%d in ('dir /a:d /o:-n /s ..\..\packages\DynamicsCrm.DevKit.Cli.* /b') do if not defined DynamicsCrmDevKitCli set DynamicsCrmDevKitCli=%%d
-set ConnectionString="AuthType=ClientSecret;Url=https://dynamics-crm-devkit-v4.crm.dynamics.com;ClientId=1a60a5c2-d04c-4b26-8f86-9d6ce0616799;ClientSecret=4Y11hDyKJYQTqXC9cRDXnoJ2DytZDs/jYI1byYwKli57mRfjHcCPu6Qx5sxgtCWQ;"
-"%DynamicsCrmDevKitCli%\tools\DynamicsCrm.DevKit.Cli.exe" /conn:%ConnectionString% /json:"..\..\DynamicsCrm.DevKit.Cli.json" /type:"generators" /profile:"TS-FORM"
+devkit generator --url "https://dynamics-crm-devkit-v4.crm.dynamics.com" --auth ClientSecret --clientid "1a60a5c2-d04c-4b26-8f86-9d6ce0616799" --clientsecret "4Y11hDyKJYQTqXC9cRDXnoJ2DytZDs/jYI1byYwKli57mRfjHcCPu6Qx5sxgtCWQ" --json "..\..\DynamicsCrm.DevKit.Cli.json" --profile "TS-FORM"
--- v5/DynamicsCrm.DevKit.Tests/TestServerCode/TestPluginServer/Dev.DevKit.Server/deploy.debug.bat
+++ v5/DynamicsCrm.DevKit.Tests/TestServerCode/TestPluginServer/Dev.DevKit.Server/deploy.debug.bat
@@ -1,4 +1,2 @@
 @echo off
-for /f "delims=" %%d in ('dir /a:d /o:-n /s ..\packages\DynamicsCrm.DevKit.Cli.* /b') do if not defined DynamicsCrmDevKitCli set DynamicsCrmDevKitCli=%%d
-set ConnectionString="AuthType=ClientSecret;Url=https://dynamics-crm-devkit-v4.crm.dynamics.com;ClientId=1a60a5c2-d04c-4b26-8f86-9d6ce0616799;ClientSecret=4Y11hDyKJYQTqXC9cRDXnoJ2DytZDs/jYI1byYwKli57mRfjHcCPu6Qx5sxgtCWQ;"
-devkit /conn:%ConnectionString% /json:"..\DynamicsCrm.DevKit.Cli.json" /type:"servers" /profile:"DEBUG"
+devkit server --url "https://dynamics-crm-devkit-v4.crm.dynamics.com" --auth ClientSecret --clientid "1a60a5c2-d04c-4b26-8f86-9d6ce0616799" --clientsecret "4Y11hDyKJYQTqXC9cRDXnoJ2DytZDs/jYI1byYwKli57mRfjHcCPu6Qx5sxgtCWQ" --json "..\DynamicsCrm.DevKit.Cli.json" --profile "DEBUG"
