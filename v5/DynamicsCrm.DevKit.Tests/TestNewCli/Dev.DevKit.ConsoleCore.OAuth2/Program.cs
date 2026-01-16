faa6556994d83c22c15502e26e7d8ec8027f86a3
PhuocLe <vanphuoc@gmail.com>
2026-01-16 17:43:51 +0700

    test: add OAuth2 and Interactive2 test projects from VSIX verification

---
+++ v5/DynamicsCrm.DevKit.Tests/TestNewCli/Dev.DevKit.ConsoleCore.OAuth2/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using System;
+
+namespace Dev.DevKit.ConsoleCore.OAuth2
+{
+    public class Program
+    {
+        static void Main()
+        {
+            CheckWhoAmI();
+            //DebugPlugin();
+            //DebugWorkflow();
+        }
+
+        private static void CheckWhoAmI()
+        {
+            var UserId = ((WhoAmIResponse)App.Service.Execute(new WhoAmIRequest())).UserId;
+            System.Console.WriteLine(UserId);
+            System.Console.ReadKey();
+        }
+
+        private static void DebugPlugin()
+        {
+            //var json = "";
+            //Helper.DebugPluginWith<???>(json, App.Service);
+        }
+
+        private static void DebugWorkflow()
+        {
+            //var json = "";
+            //var inputs = new Dictionary<string, object> { };
+            //Helper.DebugWorkflowWith<???>(json, App.Service, inputs);
+        }
+    }
+}
