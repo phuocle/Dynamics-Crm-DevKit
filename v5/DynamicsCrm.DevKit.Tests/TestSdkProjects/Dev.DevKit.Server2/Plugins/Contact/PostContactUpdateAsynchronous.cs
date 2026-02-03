8f01837c03c9be771780237eebf271bef13d702b
PhuocLe <vanphuoc@gmail.com>
2026-02-03 14:42:10 +0700

    .

--- v5/DynamicsCrm.DevKit.Tests/TestSdkProjects/Dev.DevKit.Server2/Plugins/Contact/PostContactUpdateAsynchronous.cs
+++ v5/DynamicsCrm.DevKit.Tests/TestSdkProjects/Dev.DevKit.Server2/Plugins/Contact/PostContactUpdateAsynchronous.cs
@@ -2,11 +2,12 @@
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Extensions;
 using System;
+using System.Collections.Generic;
 
-namespace Dev.DevKit.Server2.Plugins.Team
+namespace Dev.DevKit.Server2.Plugins.Contact
 {
-    [CrmPluginRegistration("Create", "team", StageEnum.PostOperation, ExecutionModeEnum.Asynchronous, "", "Dev.DevKit.Server2.Plugins.Team.PostTeamCreateAsynchronous", 1, IsolationModeEnum.Sandbox, PluginType = PluginType.Plugin, DeleteAsyncOperation = true, Image1Name = "PostImage", Image1Alias = "PostImage", Image1Type = ImageTypeEnum.PostImage, Image1Attributes = "*")]
-    public class PostTeamCreateAsynchronous : IPlugin
+    [CrmPluginRegistration("Update", "contact", StageEnum.PostOperation, ExecutionModeEnum.Asynchronous, "firstname", "Dev.DevKit.Server2.Plugins.Contact.PostContactUpdateAsynchronous", 1, IsolationModeEnum.Sandbox, PluginType = PluginType.Plugin, DeleteAsyncOperation = true, Image1Name = "PreImage", Image1Alias = "PreImage", Image1Type = ImageTypeEnum.PreImage, Image1Attributes = "*", Image2Name = "PostImage", Image2Alias = "PostImage", Image2Type = ImageTypeEnum.PostImage, Image2Attributes = "*")]
+    public class PostContactUpdateAsynchronous : IPlugin
     {
         /*
         InputParameters:
@@ -15,14 +16,14 @@ namespace Dev.DevKit.Server2.Plugins.Team
             CalculateMatchCodeSynchronously    System.Boolean
             SolutionUniqueName                 System.String
             MaintainLegacyAppServerBehavior    System.Boolean
+            ConcurrencyBehavior                Microsoft.Xrm.Sdk.ConcurrencyBehavior
             ReturnRowVersion                   System.Boolean
         OutputParameters:
-            id                                 System.Guid - require
         */
 
         //private readonly string unSecureConfiguration = null;
         //private readonly string secureConfiguration = null;
-        //public PostTeamCreateAsynchronous(string unSecureConfiguration, string secureConfiguration)
+        //public PostContactUpdateAsynchronous(string unSecureConfiguration, string secureConfiguration)
         //{
         //    this.unSecureConfiguration = unSecureConfiguration;
         //    this.secureConfiguration = secureConfiguration;
@@ -33,12 +34,16 @@ namespace Dev.DevKit.Server2.Plugins.Team
             var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
             var tracing = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
             if (!int.Equals(context.Stage, (int)StageEnum.PostOperation)) throw new InvalidPluginExecutionException("Stage does not equals PostOperation");
-            if (!string.Equals(context.MessageName, "Create", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("MessageName does not equals Create");
-            if (!string.Equals(context.PrimaryEntityName, "team", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("PrimaryEntityName does not equals team");
+            if (!string.Equals(context.MessageName, "Update", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("MessageName does not equals Update");
+            if (!string.Equals(context.PrimaryEntityName, "contact", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("PrimaryEntityName does not equals contact");
             if (!int.Equals(context.Mode, (int)ExecutionModeEnum.Asynchronous)) throw new InvalidPluginExecutionException("Execution does not equals Asynchronous");
             var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
             var serviceAdmin = serviceFactory.CreateOrganizationService(null);
             var service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            var identityService = (IManagedIdentityService)serviceProvider.GetService(typeof(IManagedIdentityService));
+            var scopes = new List<string> { "https://vault.azure.net/.default" };
+            var accessToken = identityService.AcquireToken(scopes);
 
             tracing?.DebugContext(context);
 
@@ -48,6 +53,7 @@ namespace Dev.DevKit.Server2.Plugins.Team
         private void ExecutePlugin(IPluginExecutionContext context, IOrganizationServiceFactory serviceFactory, IOrganizationService serviceAdmin, IOrganizationService service, ITracingService tracing)
         {
             var targetEntity = context.InputParameterOrDefault<Entity>("Target");
+            context.PreEntityImages.TryGetValue("PreImage", out Entity preEntity);
             context.PostEntityImages.TryGetValue("PostImage", out Entity postEntity);
             //YOUR PLUGIN-CODE GO HERE
 
