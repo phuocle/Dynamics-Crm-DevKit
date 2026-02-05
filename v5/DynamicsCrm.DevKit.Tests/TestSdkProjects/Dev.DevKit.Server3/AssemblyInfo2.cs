48b5672c5d60c5fadb810f1ae549d8fc93d6e4f1
PhuocLe <vanphuoc@gmail.com>
2026-02-05 11:27:02 +0700

    .

---
+++ v5/DynamicsCrm.DevKit.Tests/TestSdkProjects/Dev.DevKit.Server3/AssemblyInfo2.cs
@@ -0,0 +1,8 @@
+﻿using Dev.DevKit.Shared;
+
+[assembly: DynamcisCrmDevKitPluginManagedIdentityAssembly(
+    TenantId = "49528483-b79b-4b88-b86e-7d882ba68911",
+    ApplicationIds = "9ae26bec-fbb3-4f94-8143-7fc0c75bf9d9",
+    CertificateFileName = "Dev.DevKit.Server3.pfx",
+    CertificatePassword = "***Dev.DevKit.Server3@@@"
+)]
