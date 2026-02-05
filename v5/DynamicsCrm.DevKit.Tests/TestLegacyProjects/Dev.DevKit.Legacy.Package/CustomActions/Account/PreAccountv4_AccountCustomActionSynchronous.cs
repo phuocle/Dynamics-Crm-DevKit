2e49577a85f0916df46efbf7ce3fd49e93a83f33
PhuocLe <vanphuoc@gmail.com>
2026-02-05 13:44:04 +0700

    .

---
+++ v5/DynamicsCrm.DevKit.Tests/TestLegacyProjects/Dev.DevKit.Legacy.Package/CustomActions/Account/PreAccountv4_AccountCustomActionSynchronous.cs
@@ -0,0 +1,61 @@
+﻿using Dev.DevKit.Legacy.Shared;
+using Microsoft.Xrm.Sdk;
+using System;
+
+namespace Dev.DevKit.Legacy.Package.CustomActions.Account
+{
+    [CrmPluginRegistration("v4_AccountCustomAction", "account", StageEnum.PreOperation, ExecutionModeEnum.Synchronous, "", "Dev.DevKit.Legacy.Package.CustomActions.Account.PreAccountv4_AccountCustomActionSynchronous", 1, IsolationModeEnum.Sandbox, PluginType = PluginType.CustomAction)]
+    public class PreAccountv4_AccountCustomActionSynchronous : IPlugin
+    {
+        /*
+        InputParameters:
+            f                   System.String - require
+            json                System.String
+            Target              Microsoft.Xrm.Sdk.EntityReference - require
+        OutputParameters:
+            output              System.String - require
+            EntityReference     Microsoft.Xrm.Sdk.EntityReference - require
+            EntityCollection    Microsoft.Xrm.Sdk.EntityCollection - require
+        */
+
+        //private readonly string unSecureConfiguration = null;
+        //private readonly string secureConfiguration = null;
+        //public PreAccountv4_AccountCustomActionSynchronous(string unSecureConfiguration, string secureConfiguration)
+        //{
+        //    this.unSecureConfiguration = unSecureConfiguration;
+        //    this.secureConfiguration = secureConfiguration;
+        //}
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var tracing = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            if (!int.Equals(context.Stage, (int)StageEnum.PreOperation)) throw new InvalidPluginExecutionException("Stage does not equals PreOperation");
+            if (!string.Equals(context.MessageName, "v4_AccountCustomAction", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("MessageName does not equals v4_AccountCustomAction");
+            if (!string.Equals(context.PrimaryEntityName, "account", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("PrimaryEntityName does not equals account");
+            if (!int.Equals(context.Mode, (int)ExecutionModeEnum.Synchronous)) throw new InvalidPluginExecutionException("Execution does not equals Synchronous");
+            var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var serviceAdmin = serviceFactory.CreateOrganizationService(null);
+            var service = serviceFactory.CreateOrganizationService(context.UserId);
+
+            tracing?.DebugContext(context);
+
+            var outputs = ExecuteCustomAction(context, serviceFactory, serviceAdmin, service, tracing);
+            foreach (var output in outputs)
+            {
+                if (context.OutputParameters.Contains(output.Key))
+                {
+                    context.OutputParameters[output.Key] = output.Value;
+                }
+            }
+        }
+
+        private ParameterCollection ExecuteCustomAction(IPluginExecutionContext context, IOrganizationServiceFactory serviceFactory, IOrganizationService serviceAdmin, IOrganizationService service, ITracingService tracing)
+        {
+            var outputs = new ParameterCollection();
+            //YOUR CUSTOM ACTION BEGIN HERE
+
+            return outputs;
+        }
+    }
+}
---
+++ v5/DynamicsCrm.DevKit.Tests/TestLegacyProjects/Dev.DevKit.Legacy.Package/Plugins/Account/PostAccountDeleteAsynchronous.cs
@@ -0,0 +1,41 @@
+﻿using Dev.DevKit.Legacy.Shared;
+using Microsoft.Xrm.Sdk;
+using Niam.XRM.Framework.Interfaces.Plugin;
+using Niam.XRM.Framework.Plugin;
+using System;
+
+namespace Dev.DevKit.Legacy.Package.Plugins.Account
+{
+    [CrmPluginRegistration("Delete", "account", StageEnum.PostOperation, ExecutionModeEnum.Asynchronous, "", "Dev.DevKit.Legacy.Package.Plugins.Account.PostAccountDeleteAsynchronous", 1, IsolationModeEnum.Sandbox, PluginType = PluginType.Plugin, DeleteAsyncOperation = true, Image1Name = "PreImage", Image1Alias = "PreImage", Image1Type = ImageTypeEnum.PreImage, Image1Attributes = "*")]
+    public class PostAccountDeleteAsynchronousPackage : PluginBase<Entity>, IPlugin
+    {
+        /*
+        InputParameters:
+            Target                 Microsoft.Xrm.Sdk.EntityReference - require
+            SolutionUniqueName     System.String
+            ConcurrencyBehavior    Microsoft.Xrm.Sdk.ConcurrencyBehavior
+        OutputParameters:
+        */
+        public PostAccountDeleteAsynchronousPackage(string unsecure, string secure) : base(unsecure, secure)
+        {
+
+        }
+        protected override void ExecuteCrmPlugin(IPluginContext<Entity> context)
+        {
+            context.TracingService.DebugContext(context.PluginExecutionContext);
+            new AccountOperation(context).Execute();
+        }
+    }
+
+    public class AccountOperation : OperationBase
+    {
+        public AccountOperation(ITransactionContext<Entity> context) : base(context)
+        {
+        }
+        protected override void HandleExecute()
+        {
+            //YOUR PLUGIN-CODE GO HERE
+
+        }
+    }
+}
