56898cf9e67285cdb120373c00fc2243f8eea065
PhuocLe <vanphuoc@gmail.com>
2026-02-06 15:50:31 +0700

    .

---
+++ v5/DynamicsCrm.DevKit.Tests/TestLegacyProjects/Dev.DevKit.Legacy.Package/DataProviders/Sqlite/DevKit/Update.cs
@@ -0,0 +1,57 @@
+﻿using Dev.DevKit.Legacy.Shared;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Extensions;
+using System;
+
+namespace Dev.DevKit.Legacy.Package.DataProviders.Sqlite.DevKit
+{
+    [CrmPluginRegistration("Dev.DevKit.Legacy.Package.DataProviders.Sqlite.DevKit.Update", "Update", PluginType.DataProvider, DataSource = "v4legacy_sqlitepackagedevkit")]
+    public class Update : IPlugin
+    {
+        /*
+          InputParameters:
+              Target                             Microsoft.Xrm.Sdk.Entity - require
+              SuppressDuplicateDetection         System.Boolean
+              CalculateMatchCodeSynchronously    System.Boolean
+              SolutionUniqueName                 System.String
+              MaintainLegacyAppServerBehavior    System.Boolean
+              ConcurrencyBehavior                Microsoft.Xrm.Sdk.ConcurrencyBehavior
+              ReturnRowVersion                   System.Boolean
+           OutputParameters:
+        */
+
+        //private readonly string unSecureConfiguration = null;
+        //private readonly string secureConfiguration = null;
+
+        //public Update(string unSecureConfiguration, string secureConfiguration)
+        //{
+        //    this.unSecureConfiguration = unSecureConfiguration;
+        //    this.secureConfiguration = secureConfiguration;
+        //}
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var tracing = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var serviceAdmin = serviceFactory.CreateOrganizationService(null);
+            var service = serviceFactory.CreateOrganizationService(context.UserId);
+            var retriever = serviceProvider.Get<IEntityDataSourceRetrieverService>();
+            var dataSource = retriever.RetrieveEntityDataSource();
+
+            tracing?.DebugContext(context);
+
+            ExecutePlugin(context, serviceFactory, serviceAdmin, service, tracing, dataSource);
+        }
+
+        private void ExecutePlugin(IPluginExecutionContext context, IOrganizationServiceFactory serviceFactory, IOrganizationService serviceAdmin, IOrganizationService service, ITracingService tracing, Entity dataSource)
+        {
+            //Get Parameter from DataSource
+            //var ??? = dataSource.GetAttributeValue<string>("???");
+            //var ??? = dataSource.GetAttributeValue<int>("???");
+            var target = context.InputParameterOrDefault<Entity>("Target");
+            //YOUR CODE ...
+
+        }
+    }
+}
--- v5/DynamicsCrm.DevKit.Tests/TestLegacyProjects/Dev.DevKit.Legacy.Package2/Plugins/Account/PostAccountUpdateAsynchronous.cs
+++ v5/DynamicsCrm.DevKit.Tests/TestLegacyProjects/Dev.DevKit.Legacy.Package2/Plugins/Account/PostAccountUpdateAsynchronous.cs
@@ -1,12 +1,13 @@
 ﻿using Dev.DevKit.Legacy.Shared;
 using Microsoft.Xrm.Sdk;
-using Microsoft.Xrm.Sdk.Extensions;
+using Niam.XRM.Framework.Interfaces.Plugin;
+using Niam.XRM.Framework.Plugin;
 using System;
 
 namespace Dev.DevKit.Legacy.Package2.Plugins.Account
 {
-    [CrmPluginRegistration("Update", "account", StageEnum.PostOperation, ExecutionModeEnum.Asynchronous, "name", "Dev.DevKit.Legacy.Package2.Plugins.Account.PostAccountUpdateAsynchronous", 1, IsolationModeEnum.Sandbox, PluginType = PluginType.Plugin, DeleteAsyncOperation = true, Image1Name = "PreImage", Image1Alias = "PreImage", Image1Type = ImageTypeEnum.PreImage, Image1Attributes = "*", Image2Name = "PostImage", Image2Alias = "PostImage", Image2Type = ImageTypeEnum.PostImage, Image2Attributes = "*")]
-    public class PostAccountUpdateAsynchronous : IPlugin
+    [CrmPluginRegistration("Update", "account", StageEnum.PostOperation, ExecutionModeEnum.Asynchronous, "firstname", "Dev.DevKit.Legacy.Package2.Plugins.Account.PostAccountUpdateAsynchronous", 1, IsolationModeEnum.Sandbox, PluginType = PluginType.Plugin, DeleteAsyncOperation = true, Image1Name = "PreImage", Image1Alias = "PreImage", Image1Type = ImageTypeEnum.PreImage, Image1Attributes = "*", Image2Name = "PostImage", Image2Alias = "PostImage", Image2Type = ImageTypeEnum.PostImage, Image2Attributes = "*")]
+    public class PostAccountUpdateAsynchronousPackage : PluginBase<Entity>, IPlugin
     {
         /*
         InputParameters:
@@ -19,37 +20,24 @@ namespace Dev.DevKit.Legacy.Package2.Plugins.Account
             ReturnRowVersion                   System.Boolean
         OutputParameters:
         */
-
-        //private readonly string unSecureConfiguration = null;
-        //private readonly string secureConfiguration = null;
-        //public PostAccountUpdateAsynchronous(string unSecureConfiguration, string secureConfiguration)
-        //{
-        //    this.unSecureConfiguration = unSecureConfiguration;
-        //    this.secureConfiguration = secureConfiguration;
-        //}
-
-        public void Execute(IServiceProvider serviceProvider)
+        public PostAccountUpdateAsynchronousPackage(string unsecure, string secure) : base(unsecure, secure)
         {
-            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
-            var tracing = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
-            if (!int.Equals(context.Stage, (int)StageEnum.PostOperation)) throw new InvalidPluginExecutionException("Stage does not equals PostOperation");
-            if (!string.Equals(context.MessageName, "Update", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("MessageName does not equals Update");
-            if (!string.Equals(context.PrimaryEntityName, "account", StringComparison.OrdinalIgnoreCase)) throw new InvalidPluginExecutionException("PrimaryEntityName does not equals account");
-            if (!int.Equals(context.Mode, (int)ExecutionModeEnum.Asynchronous)) throw new InvalidPluginExecutionException("Execution does not equals Asynchronous");
-            var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
-            var serviceAdmin = serviceFactory.CreateOrganizationService(null);
-            var service = serviceFactory.CreateOrganizationService(context.UserId);
 
-            tracing?.DebugContext(context);
-
-            ExecutePlugin(context, serviceFactory, serviceAdmin, service, tracing);
         }
-
-        private void ExecutePlugin(IPluginExecutionContext context, IOrganizationServiceFactory serviceFactory, IOrganizationService serviceAdmin, IOrganizationService service, ITracingService tracing)
+        protected override void ExecuteCrmPlugin(IPluginContext<Entity> context)
         {
-            var targetEntity = context.InputParameterOrDefault<Entity>("Target");
-            context.PreEntityImages.TryGetValue("PreImage", out Entity preEntity);
-            context.PostEntityImages.TryGetValue("PostImage", out Entity postEntity);
+            context.TracingService.DebugContext(context.PluginExecutionContext);
+            new AccountOperation(context).Execute();
+        }
+    }
+
+    public class AccountOperation : OperationBase
+    {
+        public AccountOperation(ITransactionContext<Entity> context) : base(context)
+        {
+        }
+        protected override void HandleExecute()
+        {
             //YOUR PLUGIN-CODE GO HERE
 
         }
