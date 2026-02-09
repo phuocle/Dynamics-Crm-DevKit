5a5cc886d72819c8c7a105ed3f633f7694b42df8
PhuocLe <vanphuoc@gmail.com>
2026-02-09 17:02:16 +0700

    .

---
+++ v5/DynamicsCrm.DevKit.Tests/TestLegacyProjects/Dev.DevKit.Legacy.Server/DataProviders/Sql/DevKit/Delete.cs
@@ -0,0 +1,53 @@
+﻿using Dev.DevKit.Legacy.Shared;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Extensions;
+using System;
+
+namespace Dev.DevKit.Legacy.Server.DataProviders.Sql.DevKit
+{
+    [CrmPluginRegistration("Dev.DevKit.Legacy.Server.DataProviders.Sql.DevKit.Delete", "Delete", PluginType.DataProvider, DataSource = "v4legacy_sqlserverdevkit")]
+    public class Delete : IPlugin
+    {
+        /*
+          InputParameters:
+              Target                 Microsoft.Xrm.Sdk.EntityReference - require
+              SolutionUniqueName     System.String
+              ConcurrencyBehavior    Microsoft.Xrm.Sdk.ConcurrencyBehavior
+           OutputParameters:
+        */
+
+        //private readonly string unSecureConfiguration = null;
+        //private readonly string secureConfiguration = null;
+
+        //public Delete(string unSecureConfiguration, string secureConfiguration)
+        //{
+        //    this.unSecureConfiguration = unSecureConfiguration;
+        //    this.secureConfiguration = secureConfiguration;
+        //}
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var tracing = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var serviceAdmin = serviceFactory.CreateOrganizationService(null);
+            var service = serviceFactory.CreateOrganizationService(context.UserId);
+            var retriever = serviceProvider.Get<IEntityDataSourceRetrieverService>();
+            var dataSource = retriever.RetrieveEntityDataSource();
+
+            tracing?.DebugContext(context);
+
+            ExecutePlugin(context, serviceFactory, serviceAdmin, service, tracing, dataSource);
+        }
+
+        private void ExecutePlugin(IPluginExecutionContext context, IOrganizationServiceFactory serviceFactory, IOrganizationService serviceAdmin, IOrganizationService service, ITracingService tracing, Entity dataSource)
+        {
+            //Get Parameter from DataSource
+            //var ??? = dataSource.GetAttributeValue<string>("???");
+            //var ??? = dataSource.GetAttributeValue<int>("???");
+            var target = context.InputParameterOrDefault<EntityReference>("Target");
+            //YOUR CODE ...
+
+        }
+    }
+}
