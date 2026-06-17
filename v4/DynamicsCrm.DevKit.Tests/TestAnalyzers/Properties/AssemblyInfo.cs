7b9a4e849561a4e5ebd76bac12fce5d0347d2bac
PhuocLe <vanphuoc@gmail.com>
2026-06-17 19:32:24 +0700

    Normalize text files to CRLF UTF-8
    Set repository text checkout to CRLF, normalize tracked text files in the working tree, convert the remaining ANSI AssemblyInfo file to UTF-8, and keep the existing Visual Studio project indentation change.

--- v4/DynamicsCrm.DevKit.Tests/TestAnalyzers/Properties/AssemblyInfo.cs
+++ v4/DynamicsCrm.DevKit.Tests/TestAnalyzers/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Security;
 #endif
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Dev.DevKit.Plugin.Territory")]
-[assembly: AssemblyCopyright("Copyright © 2023")]
+[assembly: AssemblyCopyright("Copyright Â© 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: SecurityRules(SecurityRuleSet.Level2)]
 [assembly: ComVisible(false)]
