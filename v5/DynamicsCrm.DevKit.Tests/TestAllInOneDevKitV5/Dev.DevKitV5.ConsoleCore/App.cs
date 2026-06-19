0a8d04e0f8f458a35c0b96492fa0a4e5e83ccdc9
PhuocLe <vanphuoc@gmail.com>
2026-06-19 09:40:14 +0700

    .

---
+++ v5/DynamicsCrm.DevKit.Tests/TestAllInOneDevKitV5/Dev.DevKitV5.ConsoleCore/App.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.PowerPlatform.Dataverse.Client;
+using System;
+using System.IO;
+
+namespace Dev.DevKitV5.ConsoleCore
+{
+    public static class App
+    {
+        private static void InitializeConfiguration()
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("AppSettings.json", optional: false, reloadOnChange: true);
+            _Configuration = configurationBuilder.Build();
+        }
+        private static IConfiguration _Configuration = null;
+        public static IConfiguration Configuration
+        {
+            get
+            {
+                if (_Configuration is null) InitializeConfiguration();
+                return _Configuration ?? throw new InvalidOperationException("Configuration could not be initialized.");
+            }
+        }
+
+        private static string GetAppSettingValue(string key)
+        {
+            var settings = Configuration.GetSection("Dataverse");
+            var value = settings.GetValue<string>(key);
+            return value ?? string.Empty;
+        }
+
+        private static ServiceClient _Service = null;
+        public static ServiceClient Service
+        {
+            get
+            {
+                if (_Service != null) return _Service;
+                _Service = new ServiceClient(ConnectionString);
+                ServiceClient.MaxConnectionTimeout = new TimeSpan(1, 0, 0);
+                return _Service;
+            }
+        }
+        private static string Url => GetAppSettingValue("Url");
+        private static string ClientId => GetAppSettingValue("ClientId");
+        private static string ClientSecret => GetAppSettingValue("ClientSecret");
+        private static string ConnectionString => $"AuthType=ClientSecret;Url={Url};ClientId={ClientId};ClientSecret={ClientSecret};";
+    }
+}
