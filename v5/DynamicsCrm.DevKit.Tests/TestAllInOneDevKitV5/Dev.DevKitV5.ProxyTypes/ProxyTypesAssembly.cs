4b4bfba434ff14273bd9602db19b1d3fe28d012c
PhuocLe <vanphuoc@gmail.com>
2026-06-19 10:54:33 +0700

    .

---
+++ v5/DynamicsCrm.DevKit.Tests/TestAllInOneDevKitV5/Dev.DevKitV5.ProxyTypes/ProxyTypesAssembly.cs
@@ -0,0 +1,6 @@
+﻿namespace Dev.DevKitV5.ProxyTypes
+{
+    public class ProxyTypesAssembly
+    {
+    }
+}
---
+++ v5/DynamicsCrm.DevKit.Tests/TestAllInOneDevKitV5/Dev.DevKitV5.UiTest/App.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.PowerPlatform.Dataverse.Client;
+using System;
+using System.Configuration;
+
+namespace Dev.DevKitV5.UiTest
+{
+    public static class App
+    {
+        private static ServiceClient _Service = null;
+        public static ServiceClient Service
+        {
+            get
+            {
+                if (_Service != null) return _Service;
+                _Service = new ServiceClient(ConnectionString);
+                ServiceClient.MaxConnectionTimeout = new TimeSpan(1, 0, 0);
+                return _Service;
+            }
+        }
+        private static string Url => ConfigurationManager.AppSettings["Url"];
+        private static string UserName => ConfigurationManager.AppSettings["UserName"];
+        private static string Password => ConfigurationManager.AppSettings["Password"];
+        private static string ConnectionString => $"AuthType=OAuth;Url={Url};Username={UserName};Password={Password};AppId=51f81489-12ee-4a9e-aaae-a2591f45987d;RedirectUri=app://58145B91-0C36-4500-8554-080854F2AC97;LoginPrompt=Auto;";
+    }
+}
