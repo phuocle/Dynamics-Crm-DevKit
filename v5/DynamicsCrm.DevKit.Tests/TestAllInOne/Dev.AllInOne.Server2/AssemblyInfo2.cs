f57ec7e4b49e528d4f3817f9f169d5a1cd725b8f
PhuocLe <vanphuoc@gmail.com>
2026-06-26 15:23:24 +0700

    Add managed identity plugin test sample

---
+++ v5/DynamicsCrm.DevKit.Tests/TestAllInOne/Dev.AllInOne.Server2/AssemblyInfo2.cs
@@ -0,0 +1,8 @@
+﻿using Dev.AllInOne.Shared;
+
+[assembly: DynamicsCrmDevKitPluginManagedIdentityAssembly(
+    TenantId = "49528483-b79b-4b88-b86e-7d882ba68911",
+    ApplicationIds = "e923278c-0071-49ae-b850-83604c672650",
+    CertificateFileName = "Dev.AllInOne.Server2.pfx",
+    CertificatePassword = "P@ssw0rd-Dev.AllInOne.Server2"
+)]
