0d0378870a9ade7a3421d40679aadc5537f7f65f
PhuocLe <vanphuoc@gmail.com>
2026-06-26 20:34:20 +0700

    .

--- v5/DynamicsCrm.DevKit.Tests/TestAllInOne/Dev.AllInOne.Server2/AssemblyInfo2.cs
+++ v5/DynamicsCrm.DevKit.Tests/TestAllInOne/Dev.AllInOne.Server2/AssemblyInfo2.cs
@@ -1,9 +1,8 @@
 ﻿using Dev.AllInOne.Shared;
 
-
 [assembly: DynamicsCrmDevKitPluginManagedIdentityAssembly(
     TenantId = "49528483-b79b-4b88-b86e-7d882ba68911",
-    ApplicationIds = "46668f5d-4f88-49d2-ab5d-a98c29acff57",
+    ApplicationIds = "3e3287bf-c44a-46ea-ae3d-057c90e4ea09",
     CertificateFileName = "Dev.AllInOne.Server2.pfx",
     CertificatePassword = "P@ssw0rd-Dev.AllInOne.Server2"
 )]
